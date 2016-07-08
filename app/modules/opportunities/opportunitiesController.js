d03cf529004a08659a7c88b943221eacd581af23
R.J. LaCount <rlacount@deloitte.com>
2016-07-08 16:36:36 -0700

    Add checkboxes and basic functionality to lists

--- app/modules/opportunities/opportunitiesController.js
+++ app/modules/opportunities/opportunitiesController.js
@@ -3,11 +3,40 @@
 module.exports =
   function opportunitiesController($scope, $log, opportunitiesService) {
     $scope.getProducts = getProducts;
+    $scope.toggle = toggle;
+    $scope.exists = exists;
+    $scope.isIndeterminate = isIndeterminate;
+    $scope.isChecked = isChecked;
 
     $scope.opportunities = opportunitiesService.get('opportunities');
     $scope.products = opportunitiesService.get('products');
 
+    $scope.items = [];
+    $scope.selected = [];
+
+    function exists(item, list) {
+      return list.indexOf(item) > -1;
+    };
+
+    function isIndeterminate() {
+      return ($scope.selected.length !== 0 && $scope.selected.length !== $scope.items.length);
+    };
+
+    function isChecked() {
+      return $scope.selected.length === $scope.items.length;
+    };
+
     function getProducts(id) {
       $log.info('ID is ' + id);
-    }
+    };
+
+    function toggle(item, list) {
+      $log.info('Item is ' + item);
+      var idx = list.indexOf(item);
+      if (idx > -1) {
+        list.splice(idx, 1);
+      } else {
+        list.push(item);
+      }
+    };
   };
