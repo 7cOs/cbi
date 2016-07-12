da56800678a92502a2cc90c56fcb965a76126f3a
R.J. LaCount <rlacount@deloitte.com>
2016-07-12 13:33:04 -0700

    Add collapse / expand / select all functionality and default button styling

--- app/modules/opportunities/opportunitiesController.js
+++ app/modules/opportunities/opportunitiesController.js
@@ -2,40 +2,47 @@
 
 module.exports =
   function opportunitiesController($scope, $log, opportunitiesService) {
-    $scope.getProducts = getProducts;
     $scope.toggle = toggle;
     $scope.exists = exists;
-    $scope.isIndeterminate = isIndeterminate;
     $scope.isChecked = isChecked;
+    $scope.toggleAll = toggleAll;
+    $scope.expandCallback = expandCallback;
+    $scope.collapseCallback = collapseCallback;
 
     $scope.opportunities = opportunitiesService.get('opportunities');
     $scope.products = opportunitiesService.get('products');
 
-    $scope.items = [];
     $scope.selected = [];
+    $scope.expandedOpportunities = [];
 
-    $scope.$on('opportunities:onReady', function () {
-      $scope.opportunities[0].isExpanded = true;
-    });
+    function expandCallback(item) {
+      $scope.expandedOpportunities.push(item);
+    };
+
+    function collapseCallback(item) {
+      var index = $scope.expandedOpportunities.indexOf(item);
+      if (index > -1) {
+        $scope.expandedOpportunities.splice(index, 1);
+      };
+    };
 
     function exists(item, list) {
       return list.indexOf(item) > -1;
     };
 
-    function isIndeterminate() {
-      return ($scope.selected.length !== 0 && $scope.selected.length !== $scope.items.length);
-    };
-
     function isChecked() {
-      return $scope.selected.length === $scope.items.length;
+      return $scope.selected.length === $scope.opportunities.length;
     };
 
-    function getProducts(id) {
-      $log.info('ID is ' + id);
+    function toggleAll() {
+      if ($scope.selected.length === $scope.opportunities.length) {
+        $scope.selected = [];
+      } else if ($scope.selected.length === 0 || $scope.selected.length > 0) {
+        $scope.selected = $scope.opportunities.slice(0);
+      }
     };
 
     function toggle(item, list) {
-      $log.info('Item is ' + item);
       var idx = list.indexOf(item);
       if (idx > -1) {
         list.splice(idx, 1);
