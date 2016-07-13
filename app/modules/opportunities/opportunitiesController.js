c07dec535f6d02e0605f7e0f1de7bc9753f48af5
wjay <wjay@deloitte.com>
2016-07-12 18:54:53 -0600

    added filter. added radio and checkbox styles, added autocomplete with filters. added chips.

--- app/modules/opportunities/opportunitiesController.js
+++ app/modules/opportunities/opportunitiesController.js
@@ -10,6 +10,10 @@ module.exports =
     $scope.toggleAll = toggleAll;
     $scope.expandCallback = expandCallback;
     $scope.collapseCallback = collapseCallback;
+    $scope.addChip = addChip;
+    $scope.removeChip = removeChip;
+    $scope.updateAccountScopeChip = updateAccountScopeChip;
+    $scope.removeFromFilterObj = removeFromFilterObj;
 
     // Get opportunities and products data
     $scope.opportunities = opportunitiesService.get('opportunities');
@@ -19,22 +23,56 @@ module.exports =
     $scope.selected = [];
     $scope.expandedOpportunities = [];
 
-    $scope.activeFilters = ['Filter 1', 'Filter 2', 'Filter 3'];
+    // Static Models - Most are used for ng-repeats on md-components
+    $scope.premises = [{name: 'On Premise'}, {name: 'Off Premise'}];
+    $scope.opportunityStatus = [{name: 'Open'}, {name: 'Targeted'}];
     $scope.opportunityType = ['All Types', 'Non-buy', 'At Risk', 'Low Velocity', 'New Placement (Quality)', 'New Placement (No Rebuy)', 'Manual'];
+    $scope.brands = [
+      {name: 'Corona Extra', size: '12 ounce Bottle'},
+      {name: 'Corona Extra', size: '12 ounce Can(s)'},
+      {name: 'Corona Light', size: '12 ounce Bottle'},
+      {name: 'Corona Light', size: '12 ounce Can(s)'}
+    ];
+    $scope.accounts = [
+      {name: 'Walmart', subAccount: 'North East'},
+      {name: 'Walmart', subAccount: 'West'},
+      {name: 'Walmart', subAccount: 'South'},
+      {name: 'Walmart', subAccount: 'East'}
+    ];
+    $scope.distributors = [
+      {name: 'Famous James\'s House of Juniper'},
+      {name: 'Famous Will\'s House of Whiskey'},
+      {name: 'Famous Eric\'s House of Eggnog'},
+      {name: 'Famous RJ\'s House of Beer'}
+    ];
 
-    $scope.filter = {
-      accountScope: '',
-      premiseType: '',
-      opportunityStatus: '',
-      opportunity: '',
-      skuBrand: '',
-      accountStore: '',
-      distributor: ''
+    // Simulated returned user data to show saved filters
+    $scope.userData = {
+      savedFilters: [{
+        name: 'Saved Filter 1',
+        filters: ['Filter 1', 'Filter 2', 'Filter 3', 'Filter 4']
+      }, {
+        name: 'Saved Filter 2',
+        filters: ['Filter 1', 'Filter 2']
+      }]
     };
+
+    // Dynamic Models
+    // Filter Model
+    $scope.filter = {
+      accountScope: false
+    };
+    // Chips model
+    $scope.activeFilters = [];
 
     // Add item to array of currently expanded list items
     function expandCallback(item) {
       $scope.expandedOpportunities.push(item);
+    }
+
+    // Public
+    function exists(item, list) {
+      return list.indexOf(item) > -1;
     };
 
     // Remove item from array of currently expanded list items
@@ -82,4 +120,49 @@ module.exports =
         item.negativeValue = true;
       }
     });
+
+    // Add a chip
+    function addChip(chip, type, onlyOneAllowed) {
+      if (chip.length > 0) {
+        if (onlyOneAllowed) $scope.removeChip(type);
+        $scope.activeFilters.push({
+          name: chip,
+          type: type
+        });
+      }
+    };
+
+    // Remove a chip
+    function removeChip(type) {
+      for (var i = 0; i < $scope.activeFilters.length; i++) {
+        if ($scope.activeFilters[i].type === type) {
+          $scope.activeFilters.splice(i, 1);
+          break;
+        }
+      }
+    };
+
+    // Add or remove the account scope chip
+    function updateAccountScopeChip() {
+      $scope.filter.accountScope === true ? $scope.removeChip('accountScope') : $scope.addChip('My Accounts Only', 'accountScope', true);
+    }
+
+    // Update model when you click on the X on the chip
+    function removeFromFilterObj(chip) {
+      $scope.filter[chip.type] = false;
+    }
+
+    // To Do: Create a better filter for brands and accounts
+    /* function querySearch(query) {
+      var results = query ? $scope.brands.filter(createFilterFor(query)) : $scope.brands;
+      return results;
+    }
+
+    // Private
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+      return function filterFn(brand) {
+        return (brand.name.indexOf(lowercaseQuery) === 0);
+      };
+    }*/
   };
