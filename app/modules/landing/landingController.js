bdb3ede2a0ae4dfc393f158c37f4c96b569f24fc
R.J. LaCount <rlacount@deloitte.com>
2016-07-25 11:26:55 -0700

    Remove scope from public methods

--- app/modules/landing/landingController.js
+++ app/modules/landing/landingController.js
@@ -4,30 +4,34 @@ module.exports =
   function accountsController($rootScope, $scope, $state, filtersService, myperformanceService, targetListService) {
     var vm = this;
 
-    // Broadcast current page name for other scopes
-    $rootScope.$broadcast('page:loaded', $state.current.name);
+    // Map public methods to scope
+    vm.isNegative = isNegative;
+    vm.isPositive = isPositive;
+    vm.ratio = ratio;
 
     vm.filter = filtersService.model;
-
     vm.performanceData = myperformanceService.model();
     vm.namedFilters = targetListService.list();
     vm.sharedFilters = targetListService.sharedList();
 
-    $scope.isNegative = function(salesData) {
+    // Broadcast current page name for other scopes
+    $rootScope.$broadcast('page:loaded', $state.current.name);
+
+    function isNegative(salesData) {
       if (salesData >= 0) {
         return false;
       }
       return true;
     };
 
-    $scope.isPositive = function(salesData) {
+    function isPositive(salesData) {
       if (salesData >= 0) {
         return true;
       }
       return false;
     };
 
-    $scope.ratio = function(closed, total) {
+    function ratio(closed, total) {
       var result = closed / total * 100;
       return result;
     };
