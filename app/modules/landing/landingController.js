fe8e8c22b0dd4c8106c54594d192115251c5f16e
James Norton <jamnorton@deloitte.com>
2016-07-25 11:11:28 -0700

    add angular to landing page, update using controllerAs syntax

--- app/modules/landing/landingController.js
+++ app/modules/landing/landingController.js
@@ -1,11 +1,34 @@
 'use strict';
 
 module.exports =
-  function accountsController($rootScope, $scope, $state, filtersService) {
+  function accountsController($rootScope, $scope, $state, filtersService, myperformanceService, targetListService) {
     var vm = this;
 
     // Broadcast current page name for other scopes
     $rootScope.$broadcast('page:loaded', $state.current.name);
 
     vm.filter = filtersService.model;
+
+    vm.performanceData = myperformanceService.model();
+    vm.namedFilters = targetListService.list();
+    vm.sharedFilters = targetListService.sharedList();
+
+    $scope.isNegative = function(salesData) {
+      if (salesData >= 0) {
+        return false;
+      }
+      return true;
+    };
+
+    $scope.isPositive = function(salesData) {
+      if (salesData >= 0) {
+        return true;
+      }
+      return false;
+    };
+
+    $scope.ratio = function(closed, total) {
+      var result = closed / total * 100;
+      return result;
+    };
   };
