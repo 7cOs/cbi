80cfa84a8f250af34dd3d2f5012edcb34078a53b
Eric Schiller <erschiller@deloitte.com>
2016-07-25 11:28:16 -0700

    Proxy pass through

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,21 @@
 
 module.exports = function(app) {
 
+  var redis = require('redis');
+  var requestProxy = require('express-request-proxy');
+  require('redis-streams')(redis);
+
   // RENDER ALL PAGES TO APP VIEW
-  app.get('*', function (req, res) {
+  app.get('/', function (req, res) {
     res.render('main', {
       config: app.get('config')
     });
   });
+
+  app.get('/api/:resource', requestProxy({
+    cache: redis.createClient(),
+    cacheMaxAge: 60,
+    url: "http://jsonplaceholder.typicode.com/:resource"
+  }));
 
 };
