e7dd1f3b0408dd67cc17288eca7c069fa1cfe188
Eric Schiller <erschiller@deloitte.com>
2016-07-26 16:48:17 -0700

    oops shotgun fix

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,16 +5,15 @@ module.exports = function(app) {
   const request = require('request');
   const util = require('../_lib/util');
 
-  // RENDER ALL PAGES TO APP VIEW
-  app.get('/', function (req, res) {
-    res.render('main', {
-      config: app.get('config')
-    });
-  });
-
   app.get('/api/*', function(req, res) {
     console.log(req.url);
     var signed = util.sign(req.url);
     req.pipe(request(signed)).pipe(res);
+  });
+
+  app.get('/*', function (req, res) {
+    res.render('main', {
+      config: app.get('config')
+    });
   });
 };
