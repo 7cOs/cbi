66ce12ab75f393d2c55d802ddb5166e93fa5f37c
Eric Schiller <erschiller@deloitte.com>
2016-07-26 15:51:41 -0700

    added url params back

--- server/_lib/util.js
+++ server/_lib/util.js
@@ -10,6 +10,24 @@ module.exports = {
       newstr[i] = newstr[i][0].toUpperCase() + copy;
     }
     return newstr.join(' ');
+  },
+
+  // URL Signing
+  sign: function(uri) {
+    const crypto = require('crypto-js');
+    var params = uri.split('?')[1];
+    uri = uri.split('?')[0];
+    var en = '/v2/' + uri.split('api/')[1];
+    var key = 'U1NCc2FXdGxJSFJ2SUVodlpHOXlMQ0JJYjJSdmNpd2dTRzlrYjNJc0lFaHZaRzl5Y3lCaGJtUWdTRzlrYjNKekxnPT0=';
+    var apiKey = 'test';
+    var baseUrl = 'http://cbrands-deloitte-dev.herokuapp.com';
+    var signature = crypto.enc.Base64.stringify(crypto.HmacSHA256(en + apiKey, key));
+    var url = baseUrl + en + '?signature=' + signature + '&apiKey=' + apiKey + '&useTestData=true';
+    if (params) {
+      url = url + '&' + params;
+    }
+    console.log(url);
+    return url;
   }
 
 };
