f5a0d1221b4078ccfc5c8ae6ac72db939be4720e
R.J. LaCount <rlacount@deloitte.com>
2016-07-26 11:32:50 -0700

    Move chart data and methods from Scorecards page

--- app/modules/accounts/accountsController.js
+++ app/modules/accounts/accountsController.js
@@ -1,12 +1,18 @@
 'use strict';
 
 module.exports =
-  function accountsController($rootScope, $scope, $state, $log, opportunitiesService, chipsService, filtersService, userService) {
+  function accountsController($rootScope, $scope, $state, $log, opportunitiesService, myperformanceService, chipsService, filtersService, userService) {
     var vm = this;
 
     // Services available in View
     vm.chipsService = chipsService;
     vm.filtersService = filtersService;
+
+    // Expose public methods
+    vm.isNegative = isNegative;
+    vm.isPositive = isPositive;
+
+    vm.distributionData = myperformanceService.distributionModel();
 
     // Broadcast current page name for other scopes
     $rootScope.$broadcast('page:loaded', $state.current.name);
@@ -20,5 +26,21 @@ module.exports =
         name: 'Saved Filter 2',
         filters: ['Filter 1', 'Filter 2']
       }]
+    };
+
+    // Public methods
+
+    function isNegative(salesData) {
+      if (salesData >= 0) {
+        return false;
+      }
+      return true;
+    };
+
+    function isPositive(salesData) {
+      if (salesData >= 0) {
+        return true;
+      }
+      return false;
     };
   };
