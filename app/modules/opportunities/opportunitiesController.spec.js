f94305d434e57c9da4219431aa3984841ace70fc
wjay <wjay@deloitte.com>
2016-07-27 11:57:01 -0600

    got unit tests working

--- app/modules/opportunities/opportunitiesController.spec.js
+++ app/modules/opportunities/opportunitiesController.spec.js
@@ -1,22 +1,35 @@
-describe('Unit: opportunitesController', function() {
+describe('Unit: opportunitiesController', function() {
   require('angular-mocks/ngMock');
   require('angular-ui-router');
 
   var scope, ctrl;
 
-  beforeEach(module('angular-ui-router'));
-  angular.mock.module('andromeda.modules.opportunities');
-
   beforeEach(function() {
-    inject(function($controller, $rootScope) {
+    angular.mock.module('ui.router');
+    angular.mock.module('andromeda.common.services')
+    angular.mock.module('andromeda.modules.opportunities');
+
+    inject(function($rootScope, $controller) {
       scope = $rootScope.$new();
-      ctrl = $controller('opportunitesController', {$scope: scope});
-      scope.$digest();
+      ctrl = $controller('opportunitiesController', {$scope: scope});
     });
   });
 
-  it('should have logic', function(ctrl) {
+  it('should have logic', function() {
     expect(true).toBeTruthy();
+  });
+
+  it('should not have logic', function() {
+    expect(false).toBeFalsy();
+  });
+
+  it('should have services defined', function() {
+    expect(ctrl.chipsService).not.toBeUndefined();
+    expect(typeof(ctrl.chipsService)).toEqual('object');
+    expect(ctrl.filtersService).not.toBeUndefined();
+    expect(typeof(ctrl.filtersService)).toEqual('object');
+    expect(ctrl.userService).not.toBeUndefined();
+    expect(typeof(ctrl.userService)).toEqual('object');
   });
 
 });
