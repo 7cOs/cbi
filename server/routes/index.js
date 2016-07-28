5b52e6ca2d2a3f44f212688207aa14806e760637
Eric Schiller <erschiller@deloitte.com>
2016-07-28 12:29:09 -0700

    basic auth test

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,9 +11,18 @@ module.exports = function(app) {
     req.pipe(request(signed)).pipe(res);
   });
 
+  app.get('/auth/test', function (req, res) {
+    request.post('https://ssodev.cbrands.com/ms_oauth/oauth2/endpoints/oauthservice/tokens', {
+      headers: {Authorization: 'Basic UG9ydGFsQ2xpZW50OllOeldhTEtKTkdaNXZ2bGdpWg=='},
+      form: {grant_type: 'client_credentials', scope: 'API.All'}
+    }).pipe(res);
+  });
+
   app.get('*', function (req, res) {
     res.render('main', {
       config: app.get('config')
     });
   });
+
 };
+
