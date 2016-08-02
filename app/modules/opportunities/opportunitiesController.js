a4cbc81cec4b28038365f697324f4e1c1b82c56f
wjay <wjay@deloitte.com>
2016-08-02 11:01:48 -0600

    refactored opportunities controller

--- app/modules/opportunities/opportunitiesController.js
+++ app/modules/opportunities/opportunitiesController.js
@@ -4,12 +4,13 @@ module.exports =
   function opportunitiesController($rootScope, $state, opportunitiesService, chipsService, filtersService, userService) {
     var vm = this;
 
-    // Services available in View
+    // Services exposed in View
     vm.chipsService = chipsService;
     vm.filtersService = filtersService;
     vm.userService = userService;
     vm.opportunitiesService = opportunitiesService;
 
+    // Data Init
     userService.getTargetLists().then(function(data) {
       console.log(data);
     });
@@ -28,84 +29,47 @@ module.exports =
       opportunitiesService.model.opportunities = data;
     });
 
-    // Map public methods to scope
-    vm.toggle = toggle;
-    vm.exists = exists;
-    vm.isChecked = isChecked;
-    vm.toggleAll = toggleAll;
-    vm.expandCallback = expandCallback;
-    vm.collapseCallback = collapseCallback;
-    vm.brandQuerySearch = brandQuerySearch;
-    vm.accountQuerySearch = accountQuerySearch;
-    vm.distributorQuerySearch = distributorQuerySearch;
-    vm.addOpportunity = addOpportunity;
-    vm.saveFilter = saveFilter;
-    vm.applyFilter = applyFilter;
-    vm.sortBy = sortBy;
-
-    // Broadcast current page name for other scopes
-    $rootScope.$broadcast('page:loaded', $state.current.name);
-
+    // This needs to be replaced when we get live data
     // Get opportunities and products data
     vm.opportunities = opportunitiesService.get('opportunities');
     vm.products = opportunitiesService.get('products');
 
-    // Set up arrays for tracking selected and expanded list items
-    vm.selected = [];
+    // Controller Variables
+    vm.depletionsChevron = false;
     vm.expandedOpportunities = [];
+    vm.opportunitiesChevron = false;
+    vm.reverse = false;
+    vm.segmentationChevron = false;
+    vm.selected = [];
+    // sortProperty is set to a value to be the default sort on page load
+    // set value to null to show unsorted data on page load
+    vm.sortProperty = 'store.name';
+    vm.storeChevron = true;
+
+    // Controller Methods
+    vm.accountQuerySearch = accountQuerySearch;
+    vm.addOpportunity = addOpportunity;
+    vm.applyFilter = applyFilter;
+    vm.brandQuerySearch = brandQuerySearch;
+    vm.collapseCallback = collapseCallback;
+    vm.distributorQuerySearch = distributorQuerySearch;
+    vm.exists = exists;
+    vm.expandCallback = expandCallback;
+    vm.isChecked = isChecked;
+    vm.saveFilter = saveFilter;
+    vm.sortBy = sortBy;
+    vm.toggle = toggle;
+    vm.toggleAll = toggleAll;
+
+    // Broadcast current page name for other scopes
+    $rootScope.$broadcast('page:loaded', $state.current.name);
 
     // ///////////////////////////////////////////////////////// Public Methods
-    // Add item to array of currently expanded list items
-    function expandCallback(item) {
-      vm.expandedOpportunities.push(item);
-    };
-
-    // Remove item from array of currently expanded list items
-    function collapseCallback(item) {
-      var index = vm.expandedOpportunities.indexOf(item);
-      if (index > -1) {
-        vm.expandedOpportunities.splice(index, 1);
-      };
-    };
-
-    // Check if list item exists and is selected
-    function exists(item, list) {
-      return list.indexOf(item) > -1;
-    };
-
-    // Check if all items are selected
-    function isChecked() {
-      return vm.selected.length === vm.opportunities.length;
-    };
-
-    // Select or deselect all list items
-    function toggleAll() {
-      if (vm.selected.length === vm.opportunities.length) {
-        vm.selected = [];
-      } else if (vm.selected.length === 0 || vm.selected.length > 0) {
-        vm.selected = vm.opportunities.slice(0);
-      }
-    };
-
-    // Select or deselect individual list item
-    function toggle(item, list) {
-      var idx = list.indexOf(item);
-      if (idx > -1) {
-        list.splice(idx, 1);
-      } else {
-        list.push(item);
-      }
-    };
-
-    // Set positive or negative label for trend values
-    vm.opportunities.forEach(function(item) {
-      var trend = item.depletionTrendVsYA;
-      if (trend > 0) {
-        item.positiveValue = true;
-      } else if (trend < 0) {
-        item.negativeValue = true;
-      }
-    });
+    function accountQuerySearch(searchText) {
+      // update to accounts
+      var results = filtersService.model.stores.filter(filterQuery(searchText, ['account', 'sub_account', 'store_name']));
+      return results;
+    }
 
     function addOpportunity() {
       opportunitiesService.createOpportunity().then(function(response) {
@@ -115,15 +79,22 @@ module.exports =
       });
     }
 
+    function applyFilter() {
+      // web service
+      console.log('applying filter');
+    }
+
     function brandQuerySearch(searchText) {
       var results = filtersService.model.brands.filter(filterQuery(searchText, ['name', 'brand', 'quantity']));
       return results;
     }
 
-    function accountQuerySearch(searchText) {
-      // update to accounts
-      var results = filtersService.model.stores.filter(filterQuery(searchText, ['account', 'sub_account', 'store_name']));
-      return results;
+    // Remove item from array of currently expanded list items
+    function collapseCallback(item) {
+      var index = vm.expandedOpportunities.indexOf(item);
+      if (index > -1) {
+        vm.expandedOpportunities.splice(index, 1);
+      };
     }
 
     function distributorQuerySearch(searchText) {
@@ -131,6 +102,57 @@ module.exports =
       return results;
     }
 
+    // Check if list item exists and is selected
+    function exists(item, list) {
+      return list.indexOf(item) > -1;
+    }
+
+    // Add item to array of currently expanded list items
+    function expandCallback(item) {
+      vm.expandedOpportunities.push(item);
+    }
+
+    // Check if all items are selected
+    function isChecked() {
+      return vm.selected.length === vm.opportunities.length;
+    }
+
+    function saveFilter() {
+      // web service
+      var payload = parseFilterObj();
+      // userService.saveOpportunityFilter(payload);
+    }
+
+    function sortBy(property) {
+      vm.reverse = (vm.sortProperty === property) ? !vm.reverse : false;
+      vm.sortProperty = property;
+
+      vm.storeChevron = (property === 'store.name') ? !vm.storeChevron : vm.storeChevron;
+      vm.opportunitiesChevron = (property === 'opCount') ? !vm.opportunitiesChevron : vm.opportunitiesChevron;
+      vm.depletionsChevron = (property === 'depletionsCYTD') ? !vm.depletionsChevron : vm.depletionsChevron;
+      vm.segmentationChevron = (property === 'segmentation') ? !vm.segmentationChevron : vm.storeChevron;
+    }
+
+    // Select or deselect individual list item
+    function toggle(item, list) {
+      var idx = list.indexOf(item);
+      if (idx > -1) {
+        list.splice(idx, 1);
+      } else {
+        list.push(item);
+      }
+    }
+
+    // Select or deselect all list items
+    function toggleAll() {
+      if (vm.selected.length === vm.opportunities.length) {
+        vm.selected = [];
+      } else if (vm.selected.length === 0 || vm.selected.length > 0) {
+        vm.selected = vm.opportunities.slice(0);
+      }
+    }
+
+    // Private Methods
     /**
      * @name filterQuery
      * @desc filter data using query from md-autocomplete
@@ -149,17 +171,6 @@ module.exports =
       };
     }
 
-    function saveFilter() {
-      // web service
-      var payload = parseFilterObj();
-      // userService.saveOpportunityFilter(payload);
-    }
-
-    function applyFilter() {
-      // web service
-      console.log('applying filter');
-    }
-
     function parseFilterObj() {
       var prettyPayload = {
         key: 'value'
@@ -167,24 +178,5 @@ module.exports =
       // iterate through chipsService.model and get it formatted correctly
       // probably should move to chipsService
       return prettyPayload;
-    }
-
-    // sortProperty is set to a value to be the default sort on page load
-    // set value to null to show unsorted data on page load
-    vm.sortProperty = 'store.name';
-    vm.reverse = false;
-    vm.storeChevron = true;
-    vm.opportunitiesChevron = false;
-    vm.depletionsChevron = false;
-    vm.segmentationChevron = false;
-
-    function sortBy(property) {
-      vm.reverse = (vm.sortProperty === property) ? !vm.reverse : false;
-      vm.sortProperty = property;
-
-      vm.storeChevron = (property === 'store.name') ? !vm.storeChevron : vm.storeChevron;
-      vm.opportunitiesChevron = (property === 'opCount') ? !vm.opportunitiesChevron : vm.opportunitiesChevron;
-      vm.depletionsChevron = (property === 'depletionsCYTD') ? !vm.depletionsChevron : vm.depletionsChevron;
-      vm.segmentationChevron = (property === 'segmentation') ? !vm.segmentationChevron : vm.storeChevron;
     }
   };
