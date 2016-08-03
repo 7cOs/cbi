5368d34644c9b52231689751bf8538b4fca61a9e
R.J. LaCount <rlacount@deloitte.com>
2016-08-03 13:43:31 -0700

    Move chart data to my performance service

--- app/shared/services/myperformanceService.js
+++ app/shared/services/myperformanceService.js
@@ -151,6 +151,8 @@ module.exports =
           'yearAgoBuTrend': 1,
           'vsPlanNumber': 1532,
           'vsPlanPercent': 4,
+          'velocityNumber': 827,
+          'velocityPercent': 5,
           'volumeContribution': 100,
           'volumeContributionBu': 50,
           'growthContribution': 99,
@@ -167,6 +169,8 @@ module.exports =
           'yearAgoBuTrend': 11,
           'vsPlanNumber': 152,
           'vsPlanPercent': 34,
+          'velocityNumber': 482,
+          'velocityPercent': 3,
           'volumeContribution': 100,
           'volumeContributionBu': 80,
           'growthContribution': 88,
@@ -239,6 +243,77 @@ module.exports =
       }]
     };
 
+    var chartData = [
+      {
+        'values': [
+          {
+            'label': 'Walmart #1167',
+            'value': 15,
+            'title': 'Walmart',
+            'address': '3500 Brumb... Kenosha, WA',
+            'storeNum': 1167,
+            'depletions': 47560
+          }, {
+            'label': 'Walmart #2872',
+            'value': -4,
+            'title': 'Walmart',
+            'address': '10562 Bell... , Belleville, MI',
+            'storeNum': 2872,
+            'depletions': 65879
+          }, {
+            'label': 'Walmart #166',
+            'value': 9,
+            'title': 'Walmart',
+            'address': '1433 S Sam... , Houston, MO',
+            'storeNum': 166,
+            'depletions': 45500
+          }, {
+            'label': 'Walmart #3395',
+            'value': 4,
+            'title': 'Walmart',
+            'address': '3501 S Loc... , Grand Island, NE',
+            'storeNum': 3395,
+            'depletions': 64329
+          }, {
+            'label': 'Walmart #1471',
+            'value': -10,
+            'title': 'Walmart',
+            'address': '1717 N Sha... , New London, WI',
+            'storeNum': 1471,
+            'depletions': 42943
+          }, {
+            'label': 'Walmart #1685',
+            'value': -1,
+            'title': 'Walmart',
+            'address': '1730 N Gar... , Pierre, SD',
+            'storeNum': 1685,
+            'depletions': 51211
+          }, {
+            'label': 'Walmart #2738',
+            'value': -3,
+            'title': 'Walmart',
+            'address': '400 Juncti... , Glen Carbon, IL',
+            'storeNum': 2738,
+            'depletions': 71200
+          }, {
+            'label': 'Walmart #2089',
+            'value': 11,
+            'title': 'Walmart',
+            'address': '3001 W Bro... , Coumbia, MO',
+            'storeNum': 2089,
+            'depletions': 61193
+          }, {
+            'label': 'Walmart #1198',
+            'value': 2,
+            'title': 'Walmart',
+            'address': 'W159S6530... , Muskego, WI',
+            'storeNum': 1198,
+            'depletions': 55342
+          }
+        ]
+      }
+    ];
+
     return {
 
       model: function() {
@@ -249,6 +324,9 @@ module.exports =
       },
       filter: function() {
         return filters;
+      },
+      chartData: function() {
+        return chartData;
       },
       distributionModel: function() {
         return distributionData;
