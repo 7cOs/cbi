4b035e821ae4038241a2b50ed71a47aedd67e075
wjay <wjay@deloitte.com>
2016-08-04 14:38:18 -0600

    finished filter specs

--- app/shared/filters/timeAgo.spec.js
+++ app/shared/filters/timeAgo.spec.js
@@ -1,51 +1,52 @@
 describe('Unit: timeAgo Filter', function() {
-  // require('angular-mocks/ngMock');
-
-  /* var filter;
+  var filter, yearsDown, yearsUp, months, days;
 
   beforeEach(function() {
     angular.mock.module('andromeda.common.filters');
-  });*/
 
-  /* beforeEach(inject(function(_timeAgoFilter_) {
-    filter = _timeAgoFilter_;
-  }));*/
-
-  /* beforeEach(function() {
-    inject(function($filter) {
-      filter = $filter;
+    inject(function(_timeAgoFilter_) {
+      filter = _timeAgoFilter_;
     });
-  });*/
-  /* beforeEach(angular.mock.inject(function(_$injector_) {
-    $filter = _$injector_.get('$filter');
-  }));*/
 
-  /* beforeEach(inject(function(_Filter_) {
-    filter = _timeAgoFilter_;
-  }));*/
+    // create a dynamic dates so it wont error out every month
+    yearsDown = new Date();
+    yearsDown.setYear(yearsDown.getFullYear() - 10);
+
+    yearsUp = new Date();
+    yearsUp.setYear(yearsUp.getFullYear() - 10.7);
+
+    months = new Date();
+    months.setMonth(months.getMonth() - 3);
+
+    days = new Date();
+    days.setDate(days.getDate() - 7);
+  });
 
   it('should exist', function() {
-    // expect(test('2000-11-05T13:15:30Z')).toEqual('16 years ago');
-    // console.log(filter);
-    expect(true).toBeTruthy();
+    expect(filter).toBeDefined();
+  });
+
+  it('should round to the nearest year if greater than one year ago', function() {
+    expect(filter(yearsDown)).toEqual('10 years ago');
+    expect(filter(yearsUp)).toEqual('11 years ago');
+  });
+
+  it('should only add an "s" if it is more than one month ago', function() {
+    yearsDown.setYear(yearsDown.getFullYear() + 9);
+    expect(filter(yearsDown)).toEqual('1 year ago');
+  });
+
+  it('should round to the nearest month if less than a year but more than 30 days', function() {
+    expect(filter(months)).toEqual('3 months ago');
+  });
+
+  it('should only add an "s" if it is more than one month ago', function() {
+    months.setMonth(months.getMonth() + 2);
+    expect(filter(months)).toEqual('1 month ago');
+  });
+
+  it('should round to the nearest day if less than a month', function() {
+    expect(filter(days)).toEqual('7 days ago');
   });
 
 });
-
-  /* var createFilter,
-      scope,
-      mockAPIReturn = {
-        archived: false,
-        createdAt: '2000-11-05T13:15:30Z',
-        id: 'testID1',
-        name: 'My target list',
-        opportunities: 1,
-        opportunitiesSummary: {
-          closedOpportunitiesCount: 4,
-          committedOpportunitiesCount: 3,
-          storesCount: 1,
-          targetedOpportunitiesCount: 2,
-          totalClosedDepletions: 5
-        }
-      };
-  */
