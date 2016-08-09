b4c3c0551efba0dd46b307a6f6c3702b30a5a8b0
James Norton <jamnorton@deloitte.com>
2016-08-09 12:56:39 -0700

    add sorting and person icon to target list page

--- app/shared/components/target-list-expanded/index.js
+++ app/shared/components/target-list-expanded/index.js
@@ -7,6 +7,15 @@ function ExpandedTargetListController($scope, $state) {
   vm.ratio = ratio;
   vm.selector = selector;
   vm.buttonState = 'named';
+  vm.sortBy = sortBy;
+
+  // Variables for sorting
+  vm.listChevron = true;
+  vm.collaboratorsChevron = false;
+  vm.lastUpdatedChevron = false;
+  vm.closedOpportunitiesChevron = false;
+  vm.totalOpportunitesChevron = true;
+  vm.depletionsChevron = true;
 
   function selector(tab) {
     vm.buttonState = tab;
@@ -17,34 +26,50 @@ function ExpandedTargetListController($scope, $state) {
     return result;
   };
 
+  function sortBy(property) {
+    vm.reverse = (vm.sortProperty === property) ? !vm.reverse : false;
+    vm.sortProperty = property;
+
+    vm.listChevron = (property === 'name') ? !vm.listChevron : vm.listChevron;
+    vm.collaboratorsChevron = (property === 'members.length') ? !vm.collaboratorsChevron : vm.collaboratorsChevron;
+    vm.lastUpdatedChevron = (property === 'created') ? !vm.lastUpdatedChevron : vm.lastUpdatedChevron;
+    vm.closedOpportunitiesChevron = (property === 'closedOpportunities') ? !vm.closedOpportunitiesChevron : vm.closedOpportunitiesChevron;
+    vm.totalOpportunitesChevron = (property === 'Opportunites') ? !vm.totalOpportunitesChevron : vm.totalOpportunitesChevron;
+    vm.depletionsChevron = (property === 'depletions') ? !vm.depletionsChevron : vm.depletionsChevron;
+  }
+
   vm.namedFilters = [{
-    'name': 'California - Whiskey Bars',
+    'name': 'California - Wine Shops',
     'creator': 'Will Jay',
     'members': ['James Norton', 'Rebecca Norton', 'Eric Schiller'],
     'created': 'One Minute Ago',
     'closedOpportunities': 520,
-    'opportunities': 2251
+    'opportunities': 2251,
+    'depletions': 1222
   }, {
-    'name': 'California - Wine Shops',
-    'creator': 'Pete Mitchell',
+    'name': 'Alabama - Beer Shops',
+    'creator': 'RJ LaCount',
     'members': ['Scott Moffat', 'John Rozsnyai'],
     'created': 'One Minute Ago',
     'closedOpportunities': 320,
-    'opportunities': 451
+    'opportunities': 451,
+    'depletions': 2256
   }, {
-    'name': 'California - Negroni Bars',
+    'name': 'Florida - Negroni Bars',
     'creator': 'Nick Bradsaw',
-    'members': ['Elizabeth Stephenson', 'Pearl Kitty', 'Michael Phelps'],
+    'members': ['Elizabeth Stephenson', 'Karen Wittekind', 'Michael Phelps'],
     'created': 'One Year Ago',
     'closedOpportunities': 1989,
-    'opportunities': 2251
+    'opportunities': 2251,
+    'depletions': 22879
   }, {
-    'name': 'California - Beer Stores',
+    'name': 'Oregon - Pinball Bars',
     'creator': 'RJ LaCount',
     'members': ['Jimmy Fallon', 'Seth Rogan', 'Papa Murphey', 'Neil Armstrong'],
     'created': 'One Minute Ago',
     'closedOpportunities': 587,
-    'opportunities': 2251
+    'opportunities': 3251,
+    'depletions': 22348
   }];
   vm.sharedFilters = [];
   vm.archivedFilters = [{
