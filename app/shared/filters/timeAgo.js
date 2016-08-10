cc56348fcfabe6d8faf2aeed30a653015c86cd86
wjay <wjay@deloitte.com>
2016-08-10 15:34:13 -0600

    added beefy beef beeferson to timeAgo filter

--- app/shared/filters/timeAgo.js
+++ app/shared/filters/timeAgo.js
@@ -1,29 +1,41 @@
 'use strict';
 
 module.exports =
-  function timeAgo() {
+  function timeAgo($filter) {
     /**
      * @name timeAgo
      * @desc calculate how much time has gone by since the update
      * @params {String} displayDate - date string to be converted
+     * @params {String} type - type of date - possible options are 'relative', 'relativeTime' and 'daysOnly'
      * @returns {String} - returns how many days, months, or years it has been since the displayDate
      * @memberOf orion.common.filters
      */
-    return function(displayDate) {
+    return function(displayDate, type) {
       var currentDate = new Date(),
           d = new Date(displayDate),
           returnStr = '';
 
-      var daysPast = Math.round((currentDate - d) / (1000 * 60 * 60 * 24));
+      if (type === 'daysOnly') {
+        var hoursPast = Math.round((currentDate - d) / (1000 * 60 * 60));
 
-      if (daysPast < 30) { // under 30 days, round to days
-        returnStr = daysPast + ' days ago';
-      } else if (daysPast < 365) { // less than 365 days, round to month
-        returnStr = Math.round(daysPast / 30);
-        returnStr += returnStr === 1 ? ' month ago' : ' months ago';
-      } else { // more than a year, round to year
-        returnStr = Math.round(daysPast / 365);
-        returnStr += returnStr === 1 ? ' year ago' : ' years ago';
+        if (hoursPast < 2) returnStr = hoursPast + ' hour ago';
+        else if (hoursPast < 25) returnStr = hoursPast + ' hours ago';
+        else if (hoursPast > 24) returnStr = Math.round(hoursPast / 24) + ' days ago';
+
+      } else if (type === 'relative' || type === 'relativeTime') {
+        var daysAgo = (currentDate - d) / (1000 * 60 * 60 * 24);
+
+        if (daysAgo > 7) { // 7+ days
+          returnStr = type === 'relativeTime' ? $filter('date')(d, 'MMMM d, y') + ' at ' + $filter('date')(d, 'h:mm a') : $filter('date')(d, 'longDate');
+        } else if (daysAgo > 1) { // 1 - 7 days
+          returnStr = Math.round(daysAgo) + ' days ago';
+        } else if (daysAgo < 1 && (daysAgo * 24) > 1) { // 61 minutes to 24 hours
+          var hours = Math.round(daysAgo * 24);
+          returnStr = hours < 2 ? hours + ' hour ago' : hours + ' hours ago';
+        } else { // 0-60 minutes
+          var minutes = Math.round(daysAgo * 24 * 60);
+          returnStr = minutes < 2 ? minutes + ' minute ago' : minutes + ' minutes ago';
+        }
       }
 
       return returnStr;
