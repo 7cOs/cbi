13c8913b4fe9e09277aa1f242584725338949909
James Norton <jamnorton@deloitte.com>
2016-08-09 17:12:48 -0700

    add modals to project

--- app/modules/target-list-detail/targetListDetailController.js
+++ app/modules/target-list-detail/targetListDetailController.js
@@ -1,6 +1,40 @@
 'use strict';
 
 module.exports =
-  function targetListDetailController($rootScope, $state, chipsService, filtersService) {
+  function targetListDetailController($rootScope, $state, chipsService, filtersService, $mdDialog) {
+    var vm = this;
 
+    vm.modalManageTargetList = modalManageTargetList;
+    vm.modalManageCollaborators = modalManageCollaborators;
+    vm.modalSendOpportunity = modalSendOpportunity;
+
+    function modalManageTargetList(ev) {
+      var parentEl = angular.element(document.body);
+      $mdDialog.show({
+        clickOutsideToClose: true,
+        parent: parentEl,
+        targetEvent: ev,
+        templateUrl: './app/modules/target-list-detail/modal-manage-target-list.html'
+      });
+    }
+
+    function modalManageCollaborators(ev) {
+      var parentEl = angular.element(document.body);
+      $mdDialog.show({
+        clickOutsideToClose: true,
+        parent: parentEl,
+        targetEvent: ev,
+        templateUrl: './app/modules/target-list-detail/modal-manage-collaborators.html'
+      });
+    }
+
+    function modalSendOpportunity(ev) {
+      var parentEl = angular.element(document.body);
+      $mdDialog.show({
+        clickOutsideToClose: true,
+        parent: parentEl,
+        targetEvent: ev,
+        templateUrl: './app/modules/target-list-detail/modal-send-opportunity.html'
+      });
+    }
   };
