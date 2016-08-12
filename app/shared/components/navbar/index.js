6ec5d7322eb8a5a9ea54a5428422390018513cef
R.J. LaCount <rlacount@deloitte.com>
2016-08-12 14:55:58 -0700

    Add proper links to flyout menu icons

--- app/shared/components/navbar/index.js
+++ app/shared/components/navbar/index.js
@@ -3,24 +3,8 @@
 function NavbarController($scope, $mdPanel) {
   var vm = this;
 
-  // Expose public methods
-  vm.showMenu = showMenu;
-
   // Default values
-  vm.noNotifications = 'You have no unread notifications.';
-  vm.notificationsActive = false;
-
-  // PUBLIC METHODS
-  function showMenu($mdPanel, menu) {
-    vm.notificationsActive = true;
-
-    $mdPanel.show({
-      hideDelay: 0,
-      position: 'top right',
-      scope: $scope.$new(),
-      templateUrl: './app/shared/components/navbar/panel-notifications.html'
-    });
-  };
+  vm.noNotifications = 'No unread notifications.';
 }
 
 module.exports =
