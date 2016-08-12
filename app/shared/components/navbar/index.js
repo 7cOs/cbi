85f3b3a85e4c3d693234520d6fa24bd70c02cabf
R.J. LaCount <rlacount@deloitte.com>
2016-08-12 14:34:29 -0700

    Refactor icon styles and add active states

--- app/shared/components/navbar/index.js
+++ app/shared/components/navbar/index.js
@@ -1,12 +1,32 @@
 'use strict';
 
-function NavbarController($scope) {
+function NavbarController($scope, $mdPanel) {
+  var vm = this;
 
+  // Expose public methods
+  vm.showMenu = showMenu;
+
+  // Default values
+  vm.noNotifications = 'You have no unread notifications.';
+  vm.notificationsActive = false;
+
+  // PUBLIC METHODS
+  function showMenu($mdPanel, menu) {
+    vm.notificationsActive = true;
+
+    $mdPanel.show({
+      hideDelay: 0,
+      position: 'top right',
+      scope: $scope.$new(),
+      templateUrl: './app/shared/components/navbar/panel-notifications.html'
+    });
+  };
 }
 
 module.exports =
   angular.module('orion.common.components.navbar', [])
   .component('navbar', {
     templateUrl: './app/shared/components/navbar/navbar.html',
-    controller: NavbarController
+    controller: NavbarController,
+    controllerAs: 'nb'
   });
