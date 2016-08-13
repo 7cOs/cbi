d485379a6e87955bc1318352eeaf5770ad7fc939
Eric Schiller <ericschiller@gmail.com>
2016-08-12 21:42:10 -0700

    Scott's salesforce stuff' '

--- server/routes/sfdc.js
+++ server/routes/sfdc.js
@@ -1,5 +1,26 @@
 'use strict';
 
 module.exports = function(app) {
+  var sfdc = require('../controllers/sfdc');
+
+  app.get('/sfdc/createNote', function (req, res) {
+    sfdc['createNote'](app, req, res);
+  });
+
+  app.get('/sfdc/deleteNote', function (req, res) {
+    sfdc['promiseDeleteNote'](app, req, res);
+  });
+
+  app.get('/sfdc/deleteAttach', function (req, res) {
+    sfdc['promiseDeleteAttach'](app, req, res);
+  });
+
+  app.get('/sfdc/searchAccounts', function (req, res) {
+    sfdc['promiseSearchAccounts'](app, req, res);
+  });
+
+  app.get('/sfdc/accountNotes', function (req, res) {
+    sfdc['promiseAccountNotes'](app, req, res);
+  });
 
 };
