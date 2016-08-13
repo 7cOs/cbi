5bebb92bad52820dd43af6071ee5e69a22f9f5cb
R.J. LaCount <rlacount@deloitte.com>
2016-08-13 14:46:27 -0700

    Add notification count

--- app/shared/components/navbar/index.js
+++ app/shared/components/navbar/index.js
@@ -4,12 +4,16 @@ function NavbarController($rootScope, $scope, $mdPanel, notificationsService) {
   var vm = this,
       userAgent = navigator.userAgent;
 
+  // User Agent Detection for IE fixes
   $rootScope.isIE = (/Trident\/7\./g).test(userAgent);
   $rootScope.isEdge = (/(?:\bEdge\/)(\d+)/g).test(userAgent);
 
+  // Services
+  vm.notificationsService = notificationsService.tempData();
+  vm.notifications = vm.notificationsService.notifications;
+
   // Default values
   vm.noNotifications = 'No unread notifications.';
-  vm.tempData = notificationsService.tempData();
 }
 
 module.exports =
