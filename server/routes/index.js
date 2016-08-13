e8ce68361b2a8f921e8f4b16874109bd76b023d2
Eric Schiller <erschiller@deloitte.com>
2016-08-13 15:07:17 -0700

    saml update
    logging assertion from route

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,7 @@ module.exports = function(app) {
   //  Angular routes
   app.get('*', function (req, res) {
     if (req.isAuthenticated() || process.env.NODE_ENV === 'local' || process.env.NODE_ENV === 'andromeda-dev') {
+      console.log(req.session.assertion);
       res.render('main', {
         config: app.get('config')
       });
