845558e318f39cef63eea220bc1348b6b2270f7f
wjay <wjay@deloitte.com>
2016-08-17 14:17:52 -0600

    changed request url. updated manage collaborator modal. added and updated patch and delete share but spec is wrong and server is returning 404 for patch and delete. ui is done for those. added x icon.

--- server/_lib/util.js
+++ server/_lib/util.js
@@ -20,7 +20,7 @@ module.exports = {
     var en = '/v2/' + uri.split('api/')[1];
     var key = 'U1NCc2FXdGxJSFJ2SUVodlpHOXlMQ0JJYjJSdmNpd2dTRzlrYjNJc0lFaHZaRzl5Y3lCaGJtUWdTRzlrYjNKekxnPT0=';
     var apiKey = 'test';
-    var baseUrl = 'http://cbrands-deloitte-dev.herokuapp.com';
+    var baseUrl = 'http://cbi-api-test.herokuapp.com';
     var signature = crypto.enc.Base64.stringify(crypto.HmacSHA256(en + apiKey, key));
     // var url = baseUrl + en + '?signature=' + signature + '&apiKey=' + apiKey + '&useTestData=true';
     var url = baseUrl + en + '?signature=' + signature + '&apiKey=' + apiKey;
