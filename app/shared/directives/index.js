e13d708e391902881ef8ee0e3cfc9ba9b9996f36
R.J. LaCount <rlacount@deloitte.com>
2016-08-17 13:11:39 -0700

    Make inline search a directive

--- app/shared/directives/index.js
+++ app/shared/directives/index.js
@@ -2,4 +2,5 @@
 
 module.exports =
   angular.module('orion.common.directives', [])
-    .directive('customChip', require('./customChip'));
+    .directive('customChip', require('./customChip'))
+    .directive('inlineSearch', require('./inline-search/index'));
