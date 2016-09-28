d22296b1cff6c15c51392581c8379a58e644507d
wjay <wjay@deloitte.com>
2016-09-28 10:46:43 -0600

    removed comment

--- app/shared/services/filtersService.js
+++ app/shared/services/filtersService.js
@@ -90,7 +90,6 @@ module.exports = /*  @ngInject */
         account: [],
         subaccount: [],
         accountTypes: '',
-        // brand: [],
         masterSKU: [],
         cbbdChain: [],
         cbbdContact: [],
