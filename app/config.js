0d58c04670cf864441d56be35df6abe8bbba0530
James Norton <jamnorton@deloitte.com>
2016-10-06 15:42:06 -0700

    add in IE work around

--- app/config.js
+++ app/config.js
@@ -1,7 +1,15 @@
 'use strict';
 
 module.exports = /*  @ngInject */
-  function($mdThemingProvider, $locationProvider) {
+  function($mdThemingProvider, $locationProvider, $httpProvider) {
+
+    if (!$httpProvider.defaults.headers.get) {
+      $httpProvider.defaults.headers.get = {};
+    }
+
+    // IE Workaround to correct incorrect caching by IE
+    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+
     $mdThemingProvider.theme('default')
       .primaryPalette('grey')
       .accentPalette('light-blue');
