b7b6c2d5f23e7f03d1404cee69c1905ca35620f9
Eric Schiller <ericschiller@gmail.com>
2016-11-03 08:40:18 -0700

    load auth swap

--- server/_config/environment/loadtest.js
+++ server/_config/environment/loadtest.js
@@ -29,11 +29,11 @@ module.exports = function (config) {
     key: 'U1NCc2FXdGxJSFJ2SUVodlpHOXlMQ0JJYjJSdmNpd2dTRzlrYjNJc0lFaHZaRzl5Y3lCaGJtUWdTRzlrYjNKekxnPT0=',
     apiKey: 'test',
     version: 'v2',
-    jwt: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsYXN0TmFtZSI6IldJTExJQU1TIiwiaXNzIjoiaHR0cHM6Ly9vcmlvbi5jYnJhbmRzLmNvbSIsImVtcGxveWVlSUQiOiIxMDEyMTM1IiwiZmlyc3ROYW1lIjoiQ0hSSVNUT1BIRVIiLCJncm91cGluZ0NvZGUiOiIyMzQiLCJjb3Jwb3JhdGVVc2VyIjp0cnVlLCJwZXJzb25JRCI6NTU0NSwiZXhwIjoxNDc5MDc5MDA2NDc5LCJpYXQiOjE0NzM4OTUwMDY0NzksInVzZXJHcm91cCI6WyJjYmktcm9sZS1pcS1hcHAtdXNlcnMiLCJjYmkgZW1wbG95ZWVzIiwidWctY2JpZ2RjLWJpei1yb2xlLWJ1c3VuaXR2cCIsImNiaSB1c2VycyIsImNiaS1hZGVuYWJsZWRhY2NvdW50cyJdLCJlbWFpbCI6IkNIUklTLldJTExJQU1TQENCUkFORFMuQ09NIiwic3JjVHlwZUNkIjpbIlNBTEVTX0hJRVIiXX0.qVSQ-dRcq0cMf7AuO8e9_Sjp_hseij5Z6pPinRSt4T8'
+    jwt: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsYXN0TmFtZSI6IldJTExJQU1TIiwiaXNzIjoiaHR0cHM6Ly9vcmlvbi5jYnJhbmRzLmNvbSIsImVtcGxveWVlSUQiOiIxMDEyMTM1IiwiZmlyc3ROYW1lIjoiQ0hSSVNUT1BIRVIiLCJncm91cGluZ0NvZGUiOiIyMzQiLCJjb3Jwb3JhdGVVc2VyIjp0cnVlLCJwZXJzb25JRCI6NTU0NSwiZXhwIjoxNDgzMzcxMzQwMDIyLCJpYXQiOjE0NzgxODczNDAxMDgsInVzZXJHcm91cCI6W10sImVtYWlsIjoiQ0hSSVMuV0lMTElBTVNAQ0JSQU5EUy5DT00iLCJzcmNUeXBlQ2QiOlsiU0FMRVNfSElFUiJdfQ.N6TonZWdpwHV4895JyD4S9BXXJq2miaKiJqcSvr5lN0'
   };
 
   config.auth = {
-    strategy: 'saml',
+    strategy: 'basic',
     user: {
       jwt: config.api.jwt,
       jwtmap: {
