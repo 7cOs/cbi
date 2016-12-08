0ef495d2e63975f3fe27e84f2c6ef040ad5a4baf
Eric Schiller <ericschiller@gmail.com>
2016-12-07 19:33:04 -0800

    fixed service prob

--- app/config.js
+++ app/config.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = /*  @ngInject */
-  function($mdThemingProvider, $locationProvider, $httpProvider, authInterceptorService) {
+  function($mdThemingProvider, $locationProvider, $httpProvider) {
 
     if (!$httpProvider.defaults.headers.get) {
       $httpProvider.defaults.headers.get = {};
     }
-    $httpProvider.interceptors.push('authInterceptorService');
+    $httpProvider.interceptors.push('httpInterceptorService');
 
     // IE Workaround to correct incorrect caching by IE
     $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Fri, 19 Aug 2016 05:00:00 GMT';
--- app/shared/services/httpInterceptorService.js
+++ app/shared/services/httpInterceptorService.js
@@ -1,10 +1,11 @@
 'use strict';
 
 module.exports = /*  @ngInject */
-  function httpInterceptorService($q, $location) {
+  function httpInterceptorService($q) {
     var responseError = function (rejection) {
       if (rejection.status === 403) {
-        $location.path('/auth/login');
+        console.log('redirecting');
+        window.location.href = '/auth/login';
       }
       return $q.reject(rejection);
     };
