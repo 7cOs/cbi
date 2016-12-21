74c5ea46dd545746f264ff106b70ceb0f9bf9de8
R.J. LaCount <rjlacount@gmail.com>
2016-12-21 09:11:35 -0800

    Add option to not reset opportunity list when querying opportunities

--- app/shared/services/opportunitiesService.js
+++ app/shared/services/opportunitiesService.js
@@ -28,7 +28,7 @@ module.exports = /*  @ngInject */
      * @returns {Object}
      * @memberOf cf.common.services
      */
-    function getOpportunities(opportunityID) {
+    function getOpportunities(opportunityID, dontResetList) {
       service.model.noOpportunitiesFound = false;
       if (!opportunityID) {
         // get applied filters
@@ -39,7 +39,10 @@ module.exports = /*  @ngInject */
       var opportunitiesPromise = $q.defer(),
           url = opportunityID ? apiHelperService.request('/api/opportunities/' + opportunityID) : apiHelperService.request('/api/opportunities/', filterPayload);
 
-      service.model.opportunities = [];
+      if (!dontResetList) {
+        service.model.opportunities = [];
+      }
+
       $http.get(url)
         .then(getOpportunitiesSuccess)
         .catch(getOpportunitiesFail);
