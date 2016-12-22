44b266d2ce29be97103609eae9665af45d096b12
R.J. LaCount <rjlacount@gmail.com>
2016-12-22 10:03:16 -0800

    Add ngInject comment

--- app/shared/components/target/targetListController.js
+++ app/shared/components/target/targetListController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports =
+module.exports = /*  @ngInject */
   function targetListController($scope, $state, targetListService, userService, loaderService) {
 
     // ****************
