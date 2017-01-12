89926f717065ffce10583bcf1b23ea858b599c9f
Eric Schiller <ericschiller@gmail.com>
2017-01-12 10:32:53 -0800

    off premise channel filter, changed wholesale club to 01

--- app/shared/services/filtersService.js
+++ app/shared/services/filtersService.js
@@ -191,7 +191,7 @@ module.exports = /*  @ngInject */
           {label: 'Mass Merchandiser', name: 'Mass Merchandiser', value: '08'},
           {label: 'Liquor', name: 'Liquor', value: '02'},
           {label: 'Military', name: 'Military', value: 'MF'},
-          {label: 'Wholesale Club', name: 'Wholesale Club', value: '1'},
+          {label: 'Wholesale Club', name: 'Wholesale Club', value: '01'},
           {label: 'Other', name: 'Other Trade Channel', value: 'OTHER'}
         ]
       },
