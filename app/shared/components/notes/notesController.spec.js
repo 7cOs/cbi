7a16680660419346ae8e0171756c1a9692c4145d
R.J. LaCount <rjlacount@gmail.com>
2017-01-13 17:24:19 -0800

    Comment out unused vars to avoid warnings

--- app/shared/components/notes/notesController.spec.js
+++ app/shared/components/notes/notesController.spec.js
@@ -1,5 +1,6 @@
 describe('Unit: notes controller', function() {
-  var scope, $rootScope, ctrl, $q, $httpBackend, $mdMenu, $state, userService, notesService, Upload, moment;
+  // var scope, $rootScope, ctrl, $q, $httpBackend, $mdMenu, $state, userService, notesService, Upload, moment;
+  var scope, $rootScope, ctrl;
 
   beforeEach(function() {
     angular.mock.module('ui.router');
@@ -9,7 +10,8 @@ describe('Unit: notes controller', function() {
     angular.mock.module('cf.common.components.notes');
     angular.mock.module('angularMoment');
 
-    inject(function(_$rootScope_, $controller, _$q_, _$httpBackend_, _$window_, _$state_, _$mdMenu_, _notesService_, _userService_, _Upload_, _moment_) {
+    // inject(function(_$rootScope_, $controller, _$q_, _$httpBackend_, _$window_, _$state_, _$mdMenu_, _notesService_, _userService_, _Upload_, _moment_) {
+    inject(function(_$rootScope_, $controller) {
       $rootScope = _$rootScope_;
       scope = $rootScope.$new();
       scope.analytics = {};
@@ -18,13 +20,13 @@ describe('Unit: notes controller', function() {
         $scope: scope,
         $rootscope: scope
       });
-      $q = _$q_;
-      $httpBackend = _$httpBackend_;
-      $state = _$state_;
-      $mdMenu = _$mdMenu_;
-      notesService = _notesService_;
-      userService = _userService_;
-      Upload = _Upload_;
+      // $q = _$q_;
+      // $httpBackend = _$httpBackend_;
+      // $state = _$state_;
+      // $mdMenu = _$mdMenu_;
+      // notesService = _notesService_;
+      // userService = _userService_;
+      // Upload = _Upload_;
     });
   });
 
