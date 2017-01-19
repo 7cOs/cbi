f8f6ccf63e8251b8415d71854c71ed7c64bffc8d
Matt Ritter <matt.ritter@slalom.com>
2017-01-19 14:24:10 -0600

    Adding GA events for Navigation and Notifications.

--- app/shared/components/navbar/navbarController.spec.js
+++ app/shared/components/navbar/navbarController.spec.js
@@ -4,6 +4,7 @@ describe('Unit: list controller', function() {
   beforeEach(function() {
     angular.mock.module('ui.router');
     angular.mock.module('ngMaterial');
+    angular.mock.module('angulartics');
     angular.mock.module('cf.common.services');
     angular.mock.module('cf.common.components.navbar');
     angular.mock.module('angularMoment');
