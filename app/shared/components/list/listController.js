503755359d1aa81047e733dee2aaaacc901821af
Matt Ritter <matt.ritter@slalom.com>
2017-02-10 13:28:13 -0600

    Disable 'My Accounts Only' filter for target list details navigation to accounts dashboard. Also mirror this behavior from opportunity results list, so that 7 digit store id is always sent with my accounts only disabled.

--- app/shared/components/list/listController.js
+++ app/shared/components/list/listController.js
@@ -799,22 +799,7 @@ module.exports = /*  @ngInject */
     }
 
     function getStoreToBePassedToAcct(storeDetails) {
-      var store = null,
-          id = null;
-      // If myAccountsOnly is true pass the versionedId (9 digits). As a fallback if it's null send id
-      if (filtersService.model.selected.myAccountsOnly === true) {
-        id = storeDetails.versionedId;
-        store = {
-          store: id + '|' + storeDetails.name + '|' + filtersService.model.selected.myAccountsOnly
-        };
-      } else {
-        // If myAccountsOnly is true pass the id (7 digits). As a fallback if it's null send versionedId
-        id = storeDetails.id;
-        store = {
-          store: id + '|' + storeDetails.name + '|' + filtersService.model.selected.myAccountsOnly
-        };
-      }
-      return store;
+      return {store: storeDetails.id + '|' + storeDetails.name + '|' + false};
     }
 
     function init() {
