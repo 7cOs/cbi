d108aa6b405d3d78c071baebf6849f6cdc2cd05d
Piotr Bogun <pbogun1123@gmail.com>
2017-05-22 16:30:54 -0500

    DE5511 - Fixes extra sub account param being sent with request

--- app/modules/accounts/accountsController.js
+++ app/modules/accounts/accountsController.js
@@ -1164,6 +1164,13 @@ module.exports = /*  @ngInject */
       var params = filtersService.getAppliedFilters('topBottom');
       appendBrandParametersForTopBottom(params);
       params = myperformanceService.appendFilterParametersForTopBottom(params, vm.currentTopBottomFilters, vm.filtersService.model.selected.myAccountsOnly);
+
+      // Fixes when drilling down into and clicking on a store and then retailer searching for a store causing subaccount ids
+      // being wrongly sent with the request
+      if (filtersService.model.selected.retailer === 'Store') {
+        delete params['subaccount'];
+      }
+
       vm.loadingTopBottom = true;
       params.additionalParams = getAppliedFiltersForTopBottom();
       userService.getTopBottomSnapshot(vm.currentTopBottomAcctType, params).then(function(data) {
