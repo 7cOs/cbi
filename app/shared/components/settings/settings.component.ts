6662c2a73fc6bf8221c586f430bc23ecfc2e0ade
Matt Ritter <matt.ritter@slalom.com>
2017-05-31 21:28:25 -0500

    US14307 - Add reference links to async pipe subscription comment block.

--- app/shared/components/settings/settings.component.ts
+++ app/shared/components/settings/settings.component.ts
@@ -28,6 +28,8 @@ export class SettingsComponent implements OnInit {
     // to subscribe and receive the same events. The publishReplay(1) variation of publish() ensures
     // that each new subscriber receives the last item from the observable stream as soon as it subscribes
     // (rather than receiving null, which it would otherwise get if another subscriber had first subscribed.
+    // Info about multiple async pipes on same observable: https://stackoverflow.com/a/40819423/4618864
+    // Info about publishReplay and refCount: https://stackoverflow.com/a/39604925/4618864
     this.version$ = this.store.select(state => state.compassVersion.version)
       .publishReplay(1)
       .refCount();
