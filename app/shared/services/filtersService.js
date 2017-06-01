f27dfb1eb9f3df7c9619ee9a74b5b5f1b4215e3e
Piotr Bogun <pbogun1123@gmail.com>
2017-06-01 16:03:58 -0500

    US15555 - adds tests and other minor fixes

--- app/shared/services/filtersService.js
+++ app/shared/services/filtersService.js
@@ -314,16 +314,16 @@ module.exports = /*  @ngInject */
         value: 4
       }],
       valuesVsTrend: [{
-        name: 'Top 10 (Values)',
+        name: 'Top 30 (Values)',
         value: 1
       }, {
-        name: 'Top 10 (Trend)',
+        name: 'Top 30 (Trend)',
         value: 2
       }, {
-        name: 'Bottom 10 (Values)',
+        name: 'Bottom 30 (Values)',
         value: 3
       }, {
-        name: 'Bottom 10 (Trend)',
+        name: 'Bottom 30 (Trend)',
         value: 4
       }],
       accountTypes: [{
