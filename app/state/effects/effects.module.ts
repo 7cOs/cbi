5fec2ff991f490e133703a0d2eed57133da618f9
Cody Knauer <codyknauer@gmail.com>
2017-06-08 13:14:18 -0500

    US16111 - expose/run date range effect and reducer

--- app/state/effects/effects.module.ts
+++ app/state/effects/effects.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { EffectsModule as Effects } from '@ngrx/effects';
 import { CompassVersionEffects } from './compass-version.effect';
+import { DateRangesEffects } from './date-ranges.effect';
 
 @NgModule({
   imports: [
-    Effects.run(CompassVersionEffects)
+    Effects.run(CompassVersionEffects),
+    Effects.run(DateRangesEffects)
   ]
 })
-export class EffectsModule {
-}
+export class EffectsModule { }
