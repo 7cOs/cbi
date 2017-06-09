ae00190f45812f4fa3bd586a900a968f4010babc
Cody Knauer <codyknauer@gmail.com>
2017-06-09 13:42:48 -0500

    US16111 - date ranges actions spec

---
+++ app/state/actions/date-ranges.action.spec.ts
@@ -0,0 +1,59 @@
+import * as DateRangesActions from './date-ranges.action';
+import { DateRange } from '../../models/date-range.model';
+import { dateRangeMock } from '../../models/date-range.model.mock';
+import * as Chance from 'chance';
+let chance = new Chance();
+
+describe('Date Ranges Actions', () => {
+
+  describe('FetchDateRangesAction', () => {
+    let action: DateRangesActions.FetchDateRangesAction;
+
+    beforeEach(() => {
+      action = new DateRangesActions.FetchDateRangesAction();
+    });
+
+    it('should have the correct type', () => {
+      expect(DateRangesActions.FETCH_DATE_RANGES_ACTION).toBe('[Date Ranges] FETCH_DATE_RANGES_ACTION');
+      expect(action.type).toBe(DateRangesActions.FETCH_DATE_RANGES_ACTION);
+    });
+  });
+
+  describe('FetchVersionSuccessAction', () => {
+    const dateRange1: DateRange = dateRangeMock();
+    const dateRange2: DateRange = dateRangeMock();
+    const dateRanges: DateRange[] = [dateRange1, dateRange2];
+    let action: DateRangesActions.FetchDateRangesSuccessAction;
+
+    beforeEach(() => {
+      action = new DateRangesActions.FetchDateRangesSuccessAction(dateRanges);
+    });
+
+    it('should have the correct type', () => {
+      expect(DateRangesActions.FETCH_DATE_RANGES_SUCCESS_ACTION).toBe('[Date Ranges] FETCH_DATE_RANGES_SUCCESS_ACTION');
+      expect(action.type).toBe(DateRangesActions.FETCH_DATE_RANGES_SUCCESS_ACTION);
+    });
+
+    it('should contain the mock payload', () => {
+      expect(action.payload).toEqual(dateRanges);
+    });
+  });
+
+  describe('FetchVersionFailureAction', () => {
+    const error: Error = new Error(chance.string());
+    let action: DateRangesActions.FetchDateRangesFailureAction;
+
+    beforeEach(() => {
+      action = new DateRangesActions.FetchDateRangesFailureAction(error);
+    });
+
+    it('should have the correct type', () => {
+      expect(DateRangesActions.FETCH_DATE_RANGES_FAILURE_ACTION).toBe('[Date Ranges] FETCH_DATE_RANGES_FAILURE_ACTION');
+      expect(action.type).toBe(DateRangesActions.FETCH_DATE_RANGES_FAILURE_ACTION);
+    });
+
+    it('should contain the mock payload', () => {
+      expect(action.payload).toEqual(error);
+    });
+  });
+});
