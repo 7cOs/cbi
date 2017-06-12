5e6f4ce8d266cbd0502236c35f357c7949ec827c
Cody Knauer <codyknauer@gmail.com>
2017-06-12 09:21:11 -0500

    US16111 - date range transformer spec

---
+++ app/services/date-range-transformer.service.spec.ts
@@ -0,0 +1,285 @@
+import { Chance } from 'chance';
+import { inject, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+
+import { DateRange } from '../models/date-range.model';
+import { dateRangeMock } from '../models/date-range.model.mock';
+import { DateRangeTransformerService } from './date-range-transformer.service';
+import { dateRangeCollectionMock } from '../models/date-range-collection.mock.model';
+
+let chance = new Chance();
+
+describe('Service: DateRangeTransformerService', () => {
+  const mockDateRangeDTOs = [
+    {
+      'code': 'FYTCCM',
+      'description': 'Fiscal Year to Current Closed Month',
+      'startDate': '20170301',
+      'endDate': '20170531'
+    },
+    {
+      'code': 'L60BDL',
+      'description': 'Last 60 Days Of Current Year With Business Day Lag',
+      'startDate': '20170404',
+      'endDate': '20170602'
+    },
+    {
+      'code': 'L120YABDL',
+      'description': 'Last 120 Days Of Past Year With Business Day Lag',
+      'startDate': '20160204',
+      'endDate': '20160602'
+    },
+    {
+      'code': 'FYTPCM',
+      'description': 'Fiscal Year to Previous Closed Month',
+      'startDate': '20170301',
+      'endDate': '20170430'
+    },
+    {
+      'code': 'CMIP1YA',
+      'description': 'Month In Progress Plus 1 Month Of Past Year',
+      'startDate': '20160701',
+      'endDate': '20160731'
+    },
+    {
+      'code': 'LCMYA',
+      'description': 'Last Closed Month Of Past Year',
+      'startDate': '20160501',
+      'endDate': '20160531'
+    },
+    {
+      'code': 'CMIP1',
+      'description': 'Current Month In Progress Plus 1 Month Of Current Year',
+      'startDate': '20170601',
+      'endDate': '20170630'
+    },
+    {
+      'code': 'L90BDL',
+      'description': 'Last 90 Days Of Current Year With Business Day Lag',
+      'startDate': '20170305',
+      'endDate': '20170602'
+    },
+    {
+      'code': 'CYTM',
+      'description': 'From First Day of Current Calendar Year to Last Closed Month',
+      'startDate': '20170101',
+      'endDate': '20170531'
+    },
+    {
+      'code': 'FYTM',
+      'description': 'From First Day of Current Fiscal Year to Last Closed Month',
+      'startDate': '20170301',
+      'endDate': '20170531'
+    },
+    {
+      'code': 'FYTDBDL',
+      'description': 'Current Fiscal Year to Date With Business Day Lag',
+      'startDate': '20170301',
+      'endDate': '20170602'
+    },
+    {
+      'code': 'FYYA',
+      'description': 'Fiscal Year Year Ago',
+      'startDate': '20160301',
+      'endDate': '20170228'
+    },
+    {
+      'code': 'L3CM',
+      'description': 'Last 3 Closed Months',
+      'startDate': '20170301',
+      'endDate': '20170531'
+    },
+    {
+      'code': 'L03YA',
+      'description': 'Last 3 Months Of Past Year',
+      'startDate': '20160301',
+      'endDate': '20160301'
+    },
+    {
+      'code': 'FYTDYABDL',
+      'description': 'Past Fiscal Year to Date With Business Day Lag',
+      'startDate': '20160301',
+      'endDate': '20160602'
+    },
+    {
+      'code': 'L365YABDL',
+      'description': 'Last 365 Days Of Past Year With Business Day Lag',
+      'startDate': '20150604',
+      'endDate': '20160602'
+    },
+    {
+      'code': 'M',
+      'description': 'Current Month',
+      'startDate': '20170601',
+      'endDate': '20170630'
+    },
+    {
+      'code': 'L3M',
+      'description': 'Last 3 Months',
+      'startDate': '20170301',
+      'endDate': '20170531'
+    },
+    {
+      'code': 'L6M',
+      'description': 'Last 6 Months',
+      'startDate': '20161201',
+      'endDate': '20170531'
+    },
+    {
+      'code': 'LCM',
+      'description': 'Last Closed Month Of Current Year',
+      'startDate': '20170501',
+      'endDate': '20170531'
+    },
+    {
+      'code': 'MTD',
+      'description': 'Current Month to Date',
+      'startDate': '20170601',
+      'endDate': '20170609'
+    },
+    {
+      'code': 'CMIPBDL',
+      'description': 'Current Month In Progress Of Current Year With Business Day Lag',
+      'startDate': '20170601',
+      'endDate': '20170602'
+    },
+    {
+      'code': 'CMIP2',
+      'description': 'Current Month In Progress Plus 2 Month Of Current Year',
+      'startDate': '20170701',
+      'endDate': '20170731'
+    },
+    {
+      'code': 'L60YABDL',
+      'description': 'Last 60 Days Of Past Year With Business Day Lag',
+      'startDate': '20160404',
+      'endDate': '20160602'
+    },
+    {
+      'code': 'L120BDL',
+      'description': 'Last 120 Days Of Current Year With Business Day Lag',
+      'startDate': '20170203',
+      'endDate': '20170602'
+    },
+    {
+      'code': 'FYTMYA',
+      'description': 'From First Day of Past Fiscal Year to Last Closed Month',
+      'startDate': '20160301',
+      'endDate': '20160531'
+    },
+    {
+      'code': 'L30YABDL',
+      'description': 'Last 30 Days Of Past Year With Business Day Lag',
+      'startDate': '20160504',
+      'endDate': '20160602'
+    },
+    {
+      'code': 'CMIP2YA',
+      'description': 'Month In Progress Plus 2 Months Of Past Year',
+      'startDate': '20160801',
+      'endDate': '20160831'
+    },
+    {
+      'code': 'L365BDL',
+      'description': 'Last 365 Days Of Current Year With Business Day Lag',
+      'startDate': '20160603',
+      'endDate': '20170602'
+    },
+    {
+      'code': 'CYTDYABDL',
+      'description': 'From First Day of Past Calendar Year to Date With Business Day Lag',
+      'startDate': '20160101',
+      'endDate': '20160602'
+    },
+    {
+      'code': 'L6CM',
+      'description': 'Last 6 Closed Months',
+      'startDate': '20161201',
+      'endDate': '20170531'
+    },
+    {
+      'code': 'L12CM',
+      'description': 'Last 12 Closed Months',
+      'startDate': '20160601',
+      'endDate': '20170531'
+    },
+    {
+      'code': 'L90YABDL',
+      'description': 'Last 90 Days Of Past Year With Business Day Lag',
+      'startDate': '20160305',
+      'endDate': '20160602'
+    },
+    {
+      'code': 'L30BDL',
+      'description': 'Last 30 Days Of Current Year With Business Day Lag',
+      'startDate': '20170504',
+      'endDate': '20170602'
+    },
+    {
+      'code': 'FY',
+      'description': 'Fiscal Year',
+      'startDate': '20170301',
+      'endDate': '20180228'
+    },
+    {
+      'code': 'CMIPYABDL',
+      'description': 'Month In Progress Of Past Year With Business Day Lag',
+      'startDate': '20160601',
+      'endDate': '20160602'
+    },
+    {
+      'code': 'L03',
+      'description': 'Last 3 Months Of Current Year',
+      'startDate': '20170301',
+      'endDate': '20170531'
+    },
+    {
+      'code': 'L12M',
+      'description': 'Last 12 Months',
+      'startDate': '20160601',
+      'endDate': '20170531'
+    },
+    {
+      'code': 'FYTD',
+      'description': 'Fiscal Year to Date',
+      'startDate': '20170301',
+      'endDate': '20170609'
+    },
+    {
+      'code': 'CYTDBDL',
+      'description': 'From First Day of Current Calendar Year to Date With Business Day Lag',
+      'startDate': '20170101',
+      'endDate': '20170602'
+    },
+    {
+      'code': 'CYTMYA',
+      'description': 'From First Day of Last Calendar Year to Last Closed Month',
+      'startDate': '20160101',
+      'endDate': '20160531'
+    }
+  ];
+
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      DateRangeTransformerService
+    ]
+  }));
+
+  describe('transformDateRanges', () => {
+    let dateRangeTransformerService: DateRangeTransformerService;
+    let store: any;
+    beforeEach(inject([ DateRangeTransformerService ],
+      (_dateRangeTransformerService: DateRangeTransformerService, _store: any) => {
+        dateRangeTransformerService = _dateRangeTransformerService;
+        store = _store;
+    }));
+
+    it('should return a collection of formatted DateRanges from a collection of DateRangeDTOs', (done) => {
+      spyOn(dateRangeTransformerService, 'transformDateRanges').and.callThrough();
+      const transformedDateRanges = dateRangeTransformerService.transformDateRanges(mockDateRangeDTOs);
+      expect(transformedDateRanges).toEqual(dateRangeCollectionMock);
+      done();
+    });
+  });
+});
