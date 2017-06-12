8a2c5f158d888bac27b13c85ae0dc838f5f62090
Cody Knauer <codyknauer@gmail.com>
2017-06-11 23:01:03 -0500

    US16111 - date range spec

---
+++ app/services/date-range.service.spec.ts
@@ -0,0 +1,44 @@
+import { Chance } from 'chance';
+import { inject, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+
+import { DateRange } from '../models/date-range.model';
+import { dateRangeMock } from '../models/date-range.model.mock';
+import { DateRangeService } from './date-range.service';
+
+let chance = new Chance();
+
+describe('Service: DateRangeService', () => {
+  const mockDateRange: DateRange = dateRangeMock();
+  const mockStore = {
+    select: jasmine.createSpy('select').and.returnValue(Observable.of(mockDateRange))
+  };
+
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      DateRangeService,
+      {
+        provide: Store,
+        useValue: mockStore
+      }
+    ]
+  }));
+
+  describe('getDateRange', () => {
+    let dateRangeService: DateRangeService;
+    let store: any;
+    beforeEach(inject([ DateRangeService, Store ],
+      (_dateRangeService: DateRangeService, _store: any) => {
+        dateRangeService = _dateRangeService;
+        store = _store;
+    }));
+
+    it('should return a date range from the store', (done) => {
+      spyOn(dateRangeService, 'getDateRange').and.callThrough();
+      dateRangeService.getDateRange('MTD');
+      expect(store.select).toHaveBeenCalled();
+      done();
+    });
+  });
+});
