0afa1ac390f2c8862ca19386d0cd8c9ada35c782
Edie Liao <ediel@slalom.com>
2017-06-22 16:35:18 -0500

    US16292 - Adding the create Note test to the smoke test suite

---
+++ automation-test/src/test/java/com/cbrands/test/smoke/NotesTest.java
@@ -0,0 +1,13 @@
+package com.cbrands.test.smoke;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NotesTest extends BaseTestCase {
+
+  @Test(description = "Create a new Note")
+  public void createNote() {
+    Assert.fail("Create Note test not implemented");
+  }
+
+}
