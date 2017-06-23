f82515fae501c9213ba7e1a6b4c66fd0f82c265a
Piotr Bogun <pbogun1123@gmail.com>
2017-06-23 15:22:22 -0500

    US16583 - styling, cleanup

--- app/shared/components/cbi-select/cbi-select.component.ts
+++ app/shared/components/cbi-select/cbi-select.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'cbi-select',
@@ -7,44 +7,28 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 
 export class CbiSelectComponent implements OnInit {
-  @Input() options: any[];
+  @Output() onOptionSelected = new EventEmitter<any>();
+
   @Input() displayKey: string;
-  @Input() valueKey: string;
+  @Input() options: any[];
   @Input() subDisplayKey?: string;
   @Input() title?: string;
+  @Input() valueKey: string;
 
-  private _model: any;
-  private _options: any[];
-  private _currentSubValue: any;
-  private _isSelectOpen: boolean = false;
+  private currentSubValue: any;
+  private model: any;
+  // tslint:disable-next-line:no-unused-variable
+  private isSelectOpen: boolean = false;
 
   constructor() {}
 
   ngOnInit() {
-    this._options = this.initOptionData(this.options);
-    this._model = this._options[0].value;
-    if (this.subDisplayKey) this._currentSubValue = this._options[0].subDisplayValue;
-  }
-
-  private initOptionData(options: any[]): any[] {
-    return options.map(option => {
-      const _option = {
-        displayValue: option[this.displayKey],
-        value: option[this.valueKey]
-      };
-
-      if (this.subDisplayKey) _option['subDisplayValue'] = option[this.subDisplayKey];
-      return _option;
-    });
+    this.model = this.options[0][this.valueKey];
+    if (this.subDisplayKey) this.currentSubValue = this.options[0][this.subDisplayKey];
   }
 
   // tslint:disable-next-line:no-unused-variable
-  private handleOnOpen(e: Event): void {
-    this._isSelectOpen = true;
-  }
-
-  // tslint:disable-next-line:no-unused-variable
-  private handleOnClose(e: Event): void {
-    this._isSelectOpen = false;
+  private optionClicked(option: any) {
+    this.onOptionSelected.emit(option);
   }
 }
