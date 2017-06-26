62fd8ef7bc3d15dcd7c8491dd9c7b443479288ff
Edie Liao <ediel@slalom.com>
2017-06-26 16:27:17 -0500

    US16292 -Added method to click a search result after searching for a retailer

--- automation-test/src/test/java/com/cbrands/pages/AccountDashboardPage.java
+++ automation-test/src/test/java/com/cbrands/pages/AccountDashboardPage.java
@@ -1,6 +1,8 @@
 package com.cbrands.pages;
 
 import com.cbrands.helper.PropertiesCache;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,10 +11,13 @@ import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.LoadableComponent;
 import org.testng.Assert;
 
-import static com.cbrands.helper.SeleniumUtils.waitForElementToClickable;
-import static com.cbrands.helper.SeleniumUtils.waitForVisibleFluentWait;
+import java.util.List;
+
+import static com.cbrands.helper.SeleniumUtils.*;
 
 public class AccountDashboardPage extends LoadableComponent<AccountDashboardPage> {
+
+  private Log log = LogFactory.getLog(AccountDashboardPage.class);
 
   private final WebDriver driver;
 
@@ -55,6 +60,37 @@ public class AccountDashboardPage extends LoadableComponent<AccountDashboardPage
     waitForElementToClickable(searchButton, true).click();
 
     return this;
+  }
+
+  public AccountDashboardPage selectRetailerChainByName(String accountName) {
+    final WebElement resultsContainer = retailerChainFilter
+      .findElement(By.xpath(".//div[contains(@class, 'results-container')]"));
+    waitForVisibleFluentWait(resultsContainer);
+
+    final List<WebElement> results = resultsContainer.findElements(By.xpath(".//li"));
+    waitForElementsVisibleFluentWait(results);
+
+    final WebElement retailer = getFirstMatchingDropdownSearchResultByName(accountName, results);
+    if(null != retailer) {
+      retailer.click();
+    } else {
+      log.info("No retailer found by the name of " + accountName);
+    }
+
+    return this;
+  }
+
+  private WebElement getFirstMatchingDropdownSearchResultByName(String accountName, List<WebElement> results) {
+    WebElement retailer = null;
+
+    for(WebElement result : results) {
+      if(accountName.equalsIgnoreCase(result.getText())) {
+        retailer = result;
+        break;
+      }
+    }
+
+    return retailer;
   }
 
   public AccountDashboardPage applyFilters() {
