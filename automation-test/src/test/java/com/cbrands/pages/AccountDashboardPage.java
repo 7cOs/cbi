8bd253b15bcdb37bbc9b0c210c545c2412ddfed5
Edie Liao <ediel@slalom.com>
2017-06-26 11:29:44 -0500

    US16292 - Adding method to enter text for the Retailer Chain search field.

--- automation-test/src/test/java/com/cbrands/pages/AccountDashboardPage.java
+++ automation-test/src/test/java/com/cbrands/pages/AccountDashboardPage.java
@@ -1,5 +1,6 @@
 package com.cbrands.pages;
 
+import com.cbrands.helper.PropertiesCache;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,6 +8,7 @@ import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.LoadableComponent;
 import org.testng.Assert;
 
+import static com.cbrands.helper.SeleniumUtils.waitForElementToClickable;
 import static com.cbrands.helper.SeleniumUtils.waitForVisibleFluentWait;
 
 public class AccountDashboardPage extends LoadableComponent<AccountDashboardPage> {
@@ -15,6 +17,9 @@ public class AccountDashboardPage extends LoadableComponent<AccountDashboardPage
 
   @FindBy(how = How.XPATH, using = "//div[contains(@class, 'account-header')]")
   private WebElement header;
+
+  @FindBy(how = How.XPATH, using = "//inline-search[@type='chain']//input[@placeholder='Account or Subaccount Name']")
+  private WebElement retailerChainTextBox;
 
   public AccountDashboardPage(WebDriver driver) {
     this.driver = driver;
@@ -28,7 +33,14 @@ public class AccountDashboardPage extends LoadableComponent<AccountDashboardPage
 
   @Override
   protected void load() {
-    Assert.fail("The Target List detail page cannot be loaded directly without a specified Target List.");
+    driver.get(PropertiesCache.getInstance().getProperty("qa.host.address") + "/");
+  }
+
+  public AccountDashboardPage enterRetailerChainSearchText(String text) {
+    waitForElementToClickable(retailerChainTextBox, true).click();
+    retailerChainTextBox.sendKeys(text);
+
+    return this;
   }
 
 }
