04b1d2334019c08dc7a9f48a833b43700d13b3bd
Edie Liao <ediel@slalom.com>
2017-06-26 13:32:56 -0500

    US16292 -Added method to click the search button for retailer chain filter

--- automation-test/src/test/java/com/cbrands/pages/AccountDashboardPage.java
+++ automation-test/src/test/java/com/cbrands/pages/AccountDashboardPage.java
@@ -1,6 +1,7 @@
 package com.cbrands.pages;
 
 import com.cbrands.helper.PropertiesCache;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -18,8 +19,8 @@ public class AccountDashboardPage extends LoadableComponent<AccountDashboardPage
   @FindBy(how = How.XPATH, using = "//div[contains(@class, 'account-header')]")
   private WebElement header;
 
-  @FindBy(how = How.XPATH, using = "//inline-search[@type='chain']//input[@placeholder='Account or Subaccount Name']")
-  private WebElement retailerChainTextBox;
+  @FindBy(how = How.XPATH, using = "//inline-search[@type='chain']")
+  private WebElement retailerChainFilter;
 
   @FindBy(css = "md-content._md div div.ng-scope div:nth-of-type(3) div:nth-of-type(2) div.apply-filters button.btn-action")
   private WebElement applyFilters;
@@ -40,8 +41,18 @@ public class AccountDashboardPage extends LoadableComponent<AccountDashboardPage
   }
 
   public AccountDashboardPage enterRetailerChainSearchText(String text) {
+    final WebElement retailerChainTextBox = retailerChainFilter
+      .findElement(By.xpath(".//input[@placeholder='Account or Subaccount Name']"));
     waitForElementToClickable(retailerChainTextBox, true).click();
     retailerChainTextBox.sendKeys(text);
+
+    return this;
+  }
+
+  public AccountDashboardPage clickSearchForRetailerChain() {
+    final WebElement searchButton = retailerChainFilter
+      .findElement(By.xpath(".//input[contains(@class, 'submit-btn visible')]"));
+    waitForElementToClickable(searchButton, true).click();
 
     return this;
   }
