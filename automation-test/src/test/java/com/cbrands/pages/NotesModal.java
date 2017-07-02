304e44950ba880c59f0d83601e29b3b958445124
Edie Liao <ediel@slalom.com>
2017-07-02 13:44:42 -0500

    US16292 - Adding a method to click the New Notes button when there are Notes already existing.

--- automation-test/src/test/java/com/cbrands/pages/NotesModal.java
+++ automation-test/src/test/java/com/cbrands/pages/NotesModal.java
@@ -3,6 +3,7 @@ package com.cbrands.pages;
 import com.cbrands.helper.PropertiesCache;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,6 +11,8 @@ import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.LoadableComponent;
 import org.testng.Assert;
+
+import static com.cbrands.helper.SeleniumUtils.*;
 
 public class NotesModal extends LoadableComponent<NotesModal> {
 
@@ -36,5 +39,15 @@ public class NotesModal extends LoadableComponent<NotesModal> {
   @Override
   protected void isLoaded() throws Error {
     Assert.assertTrue(modalContainer.isDisplayed());
+  }
+
+	/**
+   * Clicks the add Note button when other notes already exist.
+   */
+  public NotesModal clickAddNoteButton() {
+    final WebElement addNoteButton = modalContainer.findElement(By.xpath(".//p[contains(., 'New Note')]"));
+    waitForVisibleFluentWait(addNoteButton);
+    waitForElementToClickable(addNoteButton, true).click();
+    return this;
   }
 }
