a7f8b348fa88813da9f6e6ff8f0407309847c479
Edie Liao <ediel@slalom.com>
2017-07-02 16:10:38 -0500

    US16292 - Implementing create new note test

--- automation-test/src/test/java/com/cbrands/test/smoke/NotesTest.java
+++ automation-test/src/test/java/com/cbrands/test/smoke/NotesTest.java
@@ -1,24 +1,23 @@
 package com.cbrands.test.smoke;
 
 import com.cbrands.TestUser;
-import com.cbrands.pages.AccountDashboard;
-import com.cbrands.pages.HomePage;
-import com.cbrands.pages.Login;
-import com.cbrands.pages.Logout;
+import com.cbrands.pages.*;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class NotesTest extends BaseTestCase {
+  static String current_time_stamp = new java.text.SimpleDateFormat("MM.dd.yyyy HH:mm:ss").format(new java.util.Date());
 
   private Login login;
   private Logout logout;
-  private AccountDashboard accountDashboardPage;
+  private AccountDashboardPage accountDashboardPage;
 
   @BeforeMethod
   public void setUp() {
-    final TestUser testUser = TestUser.ACTOR4;
+    final TestUser testUser = TestUser.NOTES_ACTOR;
 
     login = new Login(driver);
     logout = new Logout(driver);
@@ -28,7 +27,7 @@ public class NotesTest extends BaseTestCase {
     HomePage homePage = login.loginWithValidCredentials(testUser.userName(), testUser.password());
     Assert.assertTrue(homePage.isOnHomePage(), "Failed to log in user: " + testUser.userName());
 
-    accountDashboardPage = homePage.navigateToAccountDashboard(); //TODO replace with new Account Dashboard page object
+    accountDashboardPage = homePage.navigateToAccountDashboardPage();
   }
 
   @AfterMethod
@@ -36,9 +35,30 @@ public class NotesTest extends BaseTestCase {
     logout.logoutViaUrl();
   }
 
-  @Test(description = "Create a new Note")
-  public void createNote() {
-    Assert.fail("Create Note test not implemented");
+  @Test(description = "Create a new Note", dataProvider = "NoteData")
+  public void createNote(String retailerChainName, String noteTopic, String noteText) {
+
+    final NotesModal notesModal = accountDashboardPage
+      .enterRetailerChainSearchText(retailerChainName)
+      .clickSearchForRetailerChain()
+      .selectRetailerChainByName(retailerChainName)
+      .clickApplyFilters()
+      .drillIntoRightPanelWithName(retailerChainName)
+      .drillIntoRightPanelWithName(retailerChainName)
+      .clickNotesButton();
+
+    notesModal.clickAddNoteButton()
+      .clickNoteTopicSelector()
+      .selectNoteTopicByName(noteTopic)
+      .enterNoteText(noteText)
+      .clickSave();
+
+    Assert.assertEquals(notesModal.getTextFromFirstNote(notesModal), noteText);
+  }
+
+  @DataProvider(name = "NoteData")
+  public static Object[][] noteData() {
+    return new Object[][] { {"Taco Joint", "Distribution", "Testing create notes: " + current_time_stamp } };
   }
 
 }
