16d632b36eec5fcb3b2c6a572c409d0cd404d43f
Edie Liao <ediel@slalom.com>
2017-07-13 15:49:34 -0500

    US17072 - Removing unneeded Javadocs.

--- automation-test/src/main/java/com/cbrands/helper/PropertiesCache.java
+++ automation-test/src/main/java/com/cbrands/helper/PropertiesCache.java
@@ -5,20 +5,11 @@ import java.io.InputStream;
 import java.util.Properties;
 import java.util.Set;
 
-/**
- * The Class PropertiesCache.
- */
 public class PropertiesCache {
   private static final String BASE_AUTOMATION_PROPERTIES_FILENAME = "automation.properties";
 
-  /**
-   * The config prop.
-   */
   private final Properties configProp = new Properties();
 
-  /**
-   * Instantiates a new properties cache.
-   */
   private PropertiesCache() {
     InputStream in = this.getClass().getClassLoader().getResourceAsStream(getPropertiesFilename());
     try {
@@ -39,51 +30,22 @@ public class PropertiesCache {
     return filename;
   }
 
-  /**
-   * The Class LazyHolder.
-   */
   private static class LazyHolder {
-
-    /**
-     * The Constant INSTANCE.
-     */
     private static final PropertiesCache INSTANCE = new PropertiesCache();
   }
 
-  /**
-   * Gets the single instance of PropertiesCache.
-   *
-   * @return single instance of PropertiesCache
-   */
   public static PropertiesCache getInstance() {
     return LazyHolder.INSTANCE;
   }
 
-  /**
-   * Gets the property.
-   *
-   * @param key the key
-   * @return the property
-   */
   public String getProperty(String key) {
     return configProp.getProperty(key);
   }
 
-  /**
-   * Gets the all property names.
-   *
-   * @return the all property names
-   */
   public Set<String> getAllPropertyNames() {
     return configProp.stringPropertyNames();
   }
 
-  /**
-   * Contains key.
-   *
-   * @param key the key
-   * @return true, if successful
-   */
   public boolean containsKey(String key) {
     return configProp.containsKey(key);
   }
