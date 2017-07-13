8a5960f3e6c6067ae320c1fbd328717e6bd780bf
Edie Liao <ediel@slalom.com>
2017-07-13 15:48:39 -0500

    US17072 - Reformatting PropertiesCache

--- automation-test/src/main/java/com/cbrands/helper/PropertiesCache.java
+++ automation-test/src/main/java/com/cbrands/helper/PropertiesCache.java
@@ -11,26 +11,28 @@ import java.util.Set;
 public class PropertiesCache {
   private static final String BASE_AUTOMATION_PROPERTIES_FILENAME = "automation.properties";
 
-  /** The config prop. */
-	private final Properties configProp = new Properties();
+  /**
+   * The config prop.
+   */
+  private final Properties configProp = new Properties();
 
-	/**
-	 * Instantiates a new properties cache.
-	 */
-	private PropertiesCache() {
-		InputStream in = this.getClass().getClassLoader().getResourceAsStream(getPropertiesFilename());
-		try {
-			configProp.load(in);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+  /**
+   * Instantiates a new properties cache.
+   */
+  private PropertiesCache() {
+    InputStream in = this.getClass().getClassLoader().getResourceAsStream(getPropertiesFilename());
+    try {
+      configProp.load(in);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
   private String getPropertiesFilename() {
     String filename = BASE_AUTOMATION_PROPERTIES_FILENAME;
 
     final String suffix = System.getProperty("env");
-    if(null != suffix) {
+    if (null != suffix) {
       filename += "." + suffix;
     }
 
@@ -38,49 +40,51 @@ public class PropertiesCache {
   }
 
   /**
-	 * The Class LazyHolder.
-	 */
-	private static class LazyHolder {
+   * The Class LazyHolder.
+   */
+  private static class LazyHolder {
 
-		/** The Constant INSTANCE. */
-		private static final PropertiesCache INSTANCE = new PropertiesCache();
-	}
+    /**
+     * The Constant INSTANCE.
+     */
+    private static final PropertiesCache INSTANCE = new PropertiesCache();
+  }
 
-	/**
-	 * Gets the single instance of PropertiesCache.
-	 *
-	 * @return single instance of PropertiesCache
-	 */
-	public static PropertiesCache getInstance() {
-		return LazyHolder.INSTANCE;
-	}
+  /**
+   * Gets the single instance of PropertiesCache.
+   *
+   * @return single instance of PropertiesCache
+   */
+  public static PropertiesCache getInstance() {
+    return LazyHolder.INSTANCE;
+  }
 
-	/**
-	 * Gets the property.
-	 *
-	 * @param key the key
-	 * @return the property
-	 */
-	public String getProperty(String key) {
-		return configProp.getProperty(key);
-	}
+  /**
+   * Gets the property.
+   *
+   * @param key the key
+   * @return the property
+   */
+  public String getProperty(String key) {
+    return configProp.getProperty(key);
+  }
 
-	/**
-	 * Gets the all property names.
-	 *
-	 * @return the all property names
-	 */
-	public Set<String> getAllPropertyNames() {
-		return configProp.stringPropertyNames();
-	}
+  /**
+   * Gets the all property names.
+   *
+   * @return the all property names
+   */
+  public Set<String> getAllPropertyNames() {
+    return configProp.stringPropertyNames();
+  }
 
-	/**
-	 * Contains key.
-	 *
-	 * @param key the key
-	 * @return true, if successful
-	 */
-	public boolean containsKey(String key) {
-		return configProp.containsKey(key);
-	}
+  /**
+   * Contains key.
+   *
+   * @param key the key
+   * @return true, if successful
+   */
+  public boolean containsKey(String key) {
+    return configProp.containsKey(key);
+  }
 }
