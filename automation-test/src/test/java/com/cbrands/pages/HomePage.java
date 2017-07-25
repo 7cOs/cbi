41050e7001a6896c04849f8388ecc43707021775
Edie Liao <ediel@slalom.com>
2017-07-25 16:58:40 -0500

    US17489 - Updating deprecated navigation method to reference the new pattern of navigation.

--- automation-test/src/test/java/com/cbrands/pages/HomePage.java
+++ automation-test/src/test/java/com/cbrands/pages/HomePage.java
@@ -104,12 +104,6 @@ public class HomePage extends LoadableComponent<HomePage>{
     return userInfo.isDisplayed();
   }
 
-  public TargetListListingsPage navigateToTargetListListingsPage() {
-    driver.get(PropertiesCache.getInstance().getProperty("host.address") + "/target-lists");
-
-    return PageFactory.initElements(driver, TargetListListingsPage.class);
-  }
-
   /**
    * @deprecated This method returns a deprecated page object. Please use the new navigateToTargetListListingsPage() method
    * to navigate to the
@@ -119,7 +113,7 @@ public class HomePage extends LoadableComponent<HomePage>{
    */
   @Deprecated
   public TargetList navigateTargetList() {
-    navigateToTargetListListingsPage();
+    PageFactory.initElements(driver, TargetListListingsPage.class).goToPage();
     return PageFactory.initElements(driver, TargetList.class);
   }
 
