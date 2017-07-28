33d0582e536471fd4512b6ba26a1e28bf301d276
Cody Knauer <codyknauer@gmail.com>
2017-07-28 13:53:00 -0500

    US16921 - add data transformer for myPerformance table

---
+++ app/services/my-performance-table-data-transformer.service.ts
@@ -0,0 +1,40 @@
+import * as Chance from 'chance';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+import { MyPerformanceTableRow } from '../models/my-performance-table-row.model';
+import { RoleGroups } from '../models/role-groups.model';
+let chance = new Chance();
+
+@Injectable()
+export class MyPerformanceTableDataTransformerService {
+  // still mocking the performance data for now;
+  public transformRoleGroupTableData(roleGroups: RoleGroups): MyPerformanceTableRow[] {
+    return Object.keys(roleGroups).map((groupName: string) => {
+      return {
+        descriptionRow0: groupName + 's',
+        descriptionRow1: '',
+        metricColumn0: chance.natural({max: 1000}),
+        metricColumn1: chance.natural({max: 1000}),
+        metricColumn2: chance.natural({max: 100}),
+        ctv: chance.natural({max: 100})
+      };
+    });
+  }
+//   for setting people, we will go a level deeper
+//   (this is for example purposes only)
+//   private transformPeopleTableData(roleGroups: RoleGroups): MyPerformanceTableRow[] {
+//     return Object.keys(roleGroups).map((groupName: string) => {
+//         return roleGroups[groupName].map((group: EntityResponsibilitiesDTO[]) => {
+//             return {
+//             descriptionRow0: groupName,
+//             descriptionRow1: 'groupName',
+//             metricColumn0: chance.natural({max: 1000}),
+//             metricColumn1: chance.natural({max: 1000}),
+//             metricColumn2: chance.natural({max: 100}),
+//             ctv: 99
+//             };
+//         });
+//      });
+//   }
+}
