c591329d40c1da99616bb533f93da369656414d8
Cody Knauer <codyknauer@gmail.com>
2017-08-01 17:12:09 -0500

    US16921 - make descriptionRow1 of myPerformanceTableRow an optional property

--- app/services/my-performance-table-data-transformer.service.ts
+++ app/services/my-performance-table-data-transformer.service.ts
@@ -13,7 +13,6 @@ export class MyPerformanceTableDataTransformerService {
     return Object.keys(roleGroups).map((groupName: string) => {
       return {
         descriptionRow0: roleGroups[groupName][0].typeDisplayName,
-        descriptionRow1: '',
         metricColumn0: chance.natural({max: 1000}),
         metricColumn1: chance.natural({max: 1000}),
         metricColumn2: chance.natural({max: 100}),
