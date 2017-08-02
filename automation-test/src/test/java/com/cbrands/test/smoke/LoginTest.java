da4565cc6c6e49cae8705bc7e8e9315c8105bc57
Edie Liao <ediel@slalom.com>
2017-08-02 12:00:13 -0500

    US16856 - Updating LoginTest to use new HomePage and login method.

--- automation-test/src/test/java/com/cbrands/test/smoke/LoginTest.java
+++ automation-test/src/test/java/com/cbrands/test/smoke/LoginTest.java
@@ -1,7 +1,7 @@
 package com.cbrands.test.smoke;
 
 import com.cbrands.TestUser;
-import com.cbrands.pages.Home;
+import com.cbrands.pages.HomePage;
 import com.cbrands.pages.Login;
 import com.cbrands.pages.LogoutPage;
 import org.testng.Assert;
@@ -12,12 +12,12 @@ import org.testng.annotations.*;
  */
 public class LoginTest extends BaseTestCase {
 
-  private Login login;
+  private Login loginPage;
   private LogoutPage logoutPage;
 
   @BeforeMethod
   public void setUp() {
-    login = new Login(driver);
+    loginPage = new Login(driver);
     logoutPage = new LogoutPage(driver);
 
     log.info("\nLoading webpage...");
@@ -31,8 +31,8 @@ public class LoginTest extends BaseTestCase {
 
   @Test(dataProvider = "userCredentials", description = "Testing basic login and logout")
   public void testLogin(TestUser testUser) {
-    final Home homePage = login.loginWithValidCredentials(testUser.userName(), testUser.password());
-    Assert.assertTrue(homePage.isOnHomePage(), "Login failed for userName: " + testUser.userName());
+    final HomePage homePage = loginPage.loginAs(testUser);
+    Assert.assertTrue(homePage.isLoaded(), "Login failed for userName: " + testUser.userName());
 
     log.info("Logged in successfully as: " + testUser.userName());
   }
