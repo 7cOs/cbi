c7109cc0391e27aabeb7e84e1898a2e8e332dd5a
Edie Liao <ediel@slalom.com>
2017-08-03 16:42:51 -0500

    US17741 - Added new AccountDashboard test class. Added new test class to smoke suite.

---
+++ automation-test/src/test/java/com/cbrands/test/smoke/AccountDashboardTest.java
@@ -0,0 +1,44 @@
+package com.cbrands.test.smoke;
+
+import com.cbrands.TestUser;
+import com.cbrands.pages.AccountDashboardPage;
+import com.cbrands.pages.HomePage;
+import com.cbrands.pages.Login;
+import com.cbrands.pages.LogoutPage;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AccountDashboardTest extends BaseTestCase {
+  private Login loginPage;
+  private LogoutPage logoutPage;
+  private AccountDashboardPage accountDashboardPage;
+
+  @BeforeMethod
+  public void setUp() {
+    final TestUser testUser = TestUser.ACTOR4;
+
+    loginPage = new Login(driver);
+    logoutPage = new LogoutPage(driver);
+
+    log.info("\nLoading webpage...");
+    driver.get(webAppBaseUrl);
+    final HomePage homePage = loginPage.loginAs(testUser);
+    Assert.assertTrue(homePage.isLoaded(), "Failed to log in user: " + testUser.userName());
+
+    accountDashboardPage = PageFactory.initElements(driver, AccountDashboardPage.class);
+    accountDashboardPage.goToPage();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    logoutPage.goToPage();
+  }
+
+  @Test(description = "Search for accounts")
+  public void searchAccounts() {
+    Assert.fail("Test not implemented");
+  }
+}
