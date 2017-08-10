778b20f4ceacc4769b1abaa8d40ac37c3539049a
Cody Knauer <codyknauer@gmail.com>
2017-08-10 00:24:11 -0500

    US16922 - update my performance data transformer for role groups -> positions

--- app/services/my-performance-table-data-transformer.service.ts
+++ app/services/my-performance-table-data-transformer.service.ts
@@ -2,6 +2,8 @@ import * as Chance from 'chance';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 
+import { EntityResponsibilities } from '../models/entity-responsibilities.model';
+import { EntityPeopleType } from '../enums/entity-responsibilities.enum';
 import { MyPerformanceTableRow } from '../models/my-performance-table-row.model';
 import { RoleGroups } from '../models/role-groups.model';
 let chance = new Chance();
@@ -19,5 +21,30 @@ export class MyPerformanceTableDataTransformerService {
         ctv: chance.natural({max: 100})
       };
     });
+  }
+
+  public transformPeopleTableData(roleGroups: RoleGroups): MyPerformanceTableRow[] {
+    const groupName = Object.keys(roleGroups)[0];
+    return roleGroups[groupName].map((person: EntityResponsibilities) => {
+      return {
+        descriptionRow0: person.name,
+        metricColumn0: chance.natural({max: 1000}),
+        metricColumn1: chance.natural({max: 1000}),
+        metricColumn2: chance.natural({max: 100}),
+        ctv: chance.natural({max: 100})
+      };
+    });
+  }
+
+  public buildTotalRow(roleGroups: RoleGroups): MyPerformanceTableRow {
+    const groupName: EntityPeopleType = EntityPeopleType[Object.keys(roleGroups)[0]];
+    return {
+      descriptionRow0: 'TOTAL',
+      descriptionRow1: `${groupName}s`,
+      metricColumn0: chance.natural({max: 1000}),
+      metricColumn1: chance.natural({max: 1000}),
+      metricColumn2: chance.natural({max: 100}),
+      ctv: 100
+    };
   }
 }
