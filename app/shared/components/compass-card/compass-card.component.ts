c9f0c83feaf2aabbbb327b1341d562a67a2e2976
Cody Knauer <codyknauer@gmail.com>
2017-08-22 08:39:19 -0500

    US17945 - ga for nav clicks

--- app/shared/components/compass-card/compass-card.component.ts
+++ app/shared/components/compass-card/compass-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output, Input } from '@angular/core';
+import { Angulartics2 } from 'angulartics2';
 
 @Component({
   selector: 'compass-card',
@@ -10,11 +11,21 @@ export class CompassCardComponent {
 
   @Output() onMainActionClicked = new EventEmitter<any>();
 
+  @Input() analyticsProperties: {label: string, category: string};
   @Input() title: string;
   @Input() mainAction: string;
   @Input() iconVisible: boolean;
 
+  constructor(
+    private angulartics2: Angulartics2) { }
+
   public optionMainActionClicked(): void {
+    if (this.analyticsProperties) {
+      this.angulartics2.eventTrack.next({
+        action: 'Link Click',
+        properties: this.analyticsProperties
+      });
+    }
     this.onMainActionClicked.emit();
   }
 
