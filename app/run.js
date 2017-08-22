bf5977e9691b9dbe61c22481dc377352361fe7ad
Matt Ritter <matt.ritter@slalom.com>
2017-08-22 18:28:06 -0500

    US17705 - Create ng1 and ng4 analytics event directives making use of custom analytics service. Implement examples to prove usage. Remove analyticsHelperService and replace with ng4 service initialization. Modify user profile initialization to no longer require GA cookie. Update My Performance state to include a title for page view tracking.

--- app/run.js
+++ app/run.js
@@ -1,32 +1,14 @@
 'use strict';
 
 module.exports = /*  @ngInject */
-  function($rootScope, $cookies, $analytics, userService, encodingService, analyticsHelperService) {
+  function($rootScope, $cookies, $analytics, userService, encodingService) {
 
-    var userEncoded = $cookies.get('user'),
-      gaEncoded = $cookies.get('ga'),
-      userObj = '{}',
-      gaObj = '{}';
+    const userEncoded = $cookies.get('user');
+    let userProfile = '{}';
 
     if (userEncoded) {
-      userObj = encodingService.base64Decode(userEncoded);
+      userProfile = encodingService.base64Decode(userEncoded);
     }
 
-    if (gaEncoded) {
-      gaObj = encodingService.base64Decode(gaEncoded);
-    }
-
-    userService.model.currentUser = JSON.parse(userObj);
-    $rootScope.analytics = JSON.parse(gaObj);
-
-    // GA tracker is normally created right after analytics script loads, but
-    // the angulartics GA plugin requires GA to load load prior to angular, so
-    // we create the tracker separately here after obtaining analytics ID & user ID
-    analyticsHelperService.createTracker($rootScope.analytics.id, userService.model.currentUser.employeeID);
-
-    // custom page view tracking
-    analyticsHelperService.trackPageViews();
-
-    // fetch additional user info from SFDC and set as custom GA dimensions
-    analyticsHelperService.setCustomDimensions();
+    userService.model.currentUser = JSON.parse(userProfile);
   };
--- app/shared/directives/index.js
+++ app/shared/directives/index.js
@@ -2,6 +2,7 @@
 
 module.exports =
   angular.module('cf.common.directives', [])
+    .directive('analyticsEventOn', require('./analytics-event'))
     .directive('customChip', require('./customChip'))
     .directive('longNote', require('./longNote'))
     .directive('imageLoader', require('./imageLoader'))
