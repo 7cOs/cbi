56b068450e69e59ed9a8cd471376f90b846d672b
Matt Ritter <matt.ritter@slalom.com>
2017-08-30 15:28:35 -0500

    chore - call enableProdMode() when not running locally tp enable Angular optimizations

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,13 @@
+import { enableProdMode } from '@angular/core';
+
 import './polyfills';
 import './app.module.ng1';  // legacy ng1 code
 import './main.scss';       // global+ng1 styles
+import { Environment } from './environment';
 import { AppUpgradeAdapter } from './app.module';
+
+if (!Environment.isLocal()) {
+  enableProdMode();
+}
 
 AppUpgradeAdapter.bootstrap(document.documentElement, ['cf']);
