608957b7fca373a6e467a49481d8e2de763855ed
Piotr Bogun <pbogun1123@gmail.com>
2017-09-20 16:13:04 -0500

    US16929 - cleanup commented out code

--- server/routes/api.js
+++ server/routes/api.js
@@ -20,10 +20,6 @@ module.exports = function(app) {
         v3BaseURLKey = 'accounts';
       }
 
-      // if (req.url.match(/\/v3\/accounts.+/)) v3BaseURLKey = 'accounts';
-      // if (req.url.match(/\/v3\/positions.+/)) v3BaseURLKey = 'positions';
-      // if (req.url.match(/\/v3\/(dateRangeCodes|.+\/productMetrics)/)) v3BaseURLKey = 'productMetrics';
-
       headers['X-CBI-API-AGENT'] = util.agentHeader();
       headers['User-Agent'] = req.headers['user-agent'];
 
