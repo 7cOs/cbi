0e3070c779669cd8596ec15703307c0b7beedc50
Matt Ritter <matt.ritter@slalom.com>
2017-09-23 19:21:44 -0500

    US17455 - Randomize MyPerformanceFilter mock, and improve setup of product metrics effect unit test to set random values before each test.

--- app/models/my-performance-filter.model.mock.ts
+++ app/models/my-performance-filter.model.mock.ts
@@ -1,4 +1,4 @@
-// import * as Chance from 'chance';
+import * as Chance from 'chance';
 
 import { DateRangeTimePeriodValue } from '../enums/date-range-time-period.enum';
 import { DistributionTypeValue } from '../enums/distribution-type.enum';
@@ -6,13 +6,17 @@ import { MetricTypeValue } from '../enums/metric-type.enum';
 import { MyPerformanceFilter } from './my-performance-filter.model';
 import { PremiseTypeValue } from '../enums/premise-type.enum';
 
-// const chance = new Chance();
+const chance = new Chance();
+const metricTypeValues = Object.keys(MetricTypeValue).map(key => MetricTypeValue[key]);
+const dateRangeTimePeriodValues = Object.keys(DateRangeTimePeriodValue).map(key => DateRangeTimePeriodValue[key]);
+const premiseTypeValues = Object.keys(PremiseTypeValue).map(key => PremiseTypeValue[key]);
+const distributionTypeValues = Object.keys(DistributionTypeValue).map(key => DistributionTypeValue[key]);
 
 export function getMyPerformanceFilterMock(): MyPerformanceFilter {
   return {
-    metricType: MetricTypeValue.PointsOfDistribution,
-    dateRangeCode: DateRangeTimePeriodValue.FYTDBDL,
-    premiseType: PremiseTypeValue.On,
-    distributionType: DistributionTypeValue.simple
+    metricType: metricTypeValues[chance.integer({min: 0, max: metricTypeValues.length - 1})],
+    dateRangeCode: dateRangeTimePeriodValues[chance.integer({min: 0, max: dateRangeTimePeriodValues.length - 1})],
+    premiseType: premiseTypeValues[chance.integer({min: 0, max: premiseTypeValues.length - 1})],
+    distributionType: distributionTypeValues[chance.integer({min: 0, max: distributionTypeValues.length - 1})]
   };
 }
