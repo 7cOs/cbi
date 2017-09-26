3642d332236442135c8a22009f150499128332e1
Cody Knauer <codyknauer@gmail.com>
2017-09-26 16:13:41 -0500

    US17032 - update api config for sub accounts performance

--- server/routes/api.js
+++ server/routes/api.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
       // TODO: remove once api gateway is in place
       if (req.url.match(/\/v3\/(dateRangeCodes|.+\/productMetrics)/)) {
         v3BaseURLKey = 'productMetrics';
-      } else if (req.url.match(/\/v3\/accounts.+/)) {
+      } else if (req.url.match(/\/v3\/(accounts|.+\/subAccounts)/)) {
         v3BaseURLKey = 'accounts';
       } else if (req.url.match(/\/v3\/distributors.+/)) {
         v3BaseURLKey = 'distributors';
@@ -44,6 +44,8 @@ module.exports = function(app) {
 
       let apiRequestStream = request(auth.signed, {json: true, headers: auth.headers}).auth(null, null, true, auth.jwtToken);
       req.pipe(apiRequestStream); // pipe client request to API request so headers are passed through
+
+      if (req.url.match(/\/v3\/(accounts|.+\/subAccounts)/)) console.log('subAccounts: ', auth.signed);
 
       apiRequestStream
         .on('error', logutil.handleAPIRequestError(req, res, 'GET', auth.signed))
