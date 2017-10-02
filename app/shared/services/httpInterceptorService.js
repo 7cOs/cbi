abd5baadd0d53eecfc70ffe75b4b6a3ba3814107
Matt Ritter <matt.ritter@slalom.com>
2017-10-02 18:24:11 -0500

    chore - Use default user profile object with deeper placeholders to prevent javascript failure when user profile cookie is missing.

--- app/shared/services/httpInterceptorService.js
+++ app/shared/services/httpInterceptorService.js
@@ -3,7 +3,7 @@
 module.exports = /*  @ngInject */
   function httpInterceptorService($q) {
     var responseError = function (rejection) {
-      if (rejection.status === 401) {
+      if (rejection.status === 401 || rejection.status === 403) {
         console.log('redirecting');
         window.location.href = '/auth/expired';
       }
