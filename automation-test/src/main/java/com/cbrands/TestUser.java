ab3401d91a60edf1d2623e66decb15adfe9982a2
Edie Liao <ediel@slalom.com>
2017-10-06 15:47:06 -0500

    US18647-Adding role property to TestUser .

--- automation-test/src/main/java/com/cbrands/TestUser.java
+++ automation-test/src/main/java/com/cbrands/TestUser.java
@@ -4,19 +4,21 @@ package com.cbrands;
  * Test users for automated testing. Only valid users currently used in the ATs should be in this enum.
  */
 public enum TestUser {
-  ACTOR4("chris.williams@cbrands.com", "Corona.2016", "Chris", "Williams"),
-  NOTES_ACTOR("chelsea.hoff@cbrands.com", "Corona.2016", "Chelsea", "Hoff");
+  ACTOR4("chris.williams@cbrands.com", "Corona.2016", "Chris", "Williams", "Non-Corporate"),
+  NOTES_ACTOR("chelsea.hoff@cbrands.com", "Corona.2016", "Chelsea", "Hoff", "Non-Corporate");
 
   private final String userName;
   private final String password;
   private final String firstName;
   private final String lastName;
+  private final String role;
 
-  TestUser(String username, String password, String firstName, String lastName) {
+  TestUser(String username, String password, String firstName, String lastName, String role) {
     this.userName = username;
     this.password = password;
     this.firstName = firstName;
     this.lastName = lastName;
+    this.role = role;
   }
 
   public String userName() {
@@ -33,6 +35,10 @@ public enum TestUser {
 
   public String lastName() {
     return lastName;
+  }
+
+  public String role() {
+    return role;
   }
 
 }
