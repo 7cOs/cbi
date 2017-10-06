e506179ac282d9af7094512b7f93243e18180a4c
Michael Oguejiofor <chinedu@gmail.com>
2017-10-05 22:14:10 -0500

    US18327 - css overflow

--- app/shared/components/my-performance-table-row/my-performance-table-row.component.ts
+++ app/shared/components/my-performance-table-row/my-performance-table-row.component.ts
@@ -24,7 +24,7 @@ export class MyPerformanceTableRowComponent {
   }
 
   public columnWidth(): string {
-    return this.showOpportunities ? 'col-16-pct' : 'col-20-pct';
+    return this.showOpportunities ? 'col-17-pct' : 'col-20-pct';
   }
 
   public headerLeftClasses() {
