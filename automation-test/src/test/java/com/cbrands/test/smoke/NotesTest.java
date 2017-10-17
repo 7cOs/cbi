ccf31811b21598dfcf0b449d7e95f92f769b3091
Edie Liao <ediel@slalom.com>
2017-10-17 10:20:11 -0500

    ATFix-NotesTest-extracting opening Notes modal into a helper method in preparation for extracting Account information to DataProviders

--- automation-test/src/test/java/com/cbrands/test/smoke/NotesTest.java
+++ automation-test/src/test/java/com/cbrands/test/smoke/NotesTest.java
@@ -25,22 +25,7 @@ public class NotesTest extends BaseTestCase {
     driver.get(webAppBaseUrl);
 
     login(TestUser.NOTES_ACTOR);
-
-    final String storeAccountName = "Taco Joint";
-    final AccountDashboardPage accountDashboardPage = PageFactory.initElements(driver, AccountDashboardPage.class);
-    accountDashboardPage.goToPage();
-
-    notesModal = accountDashboardPage
-      .enterRetailerChainSearchText(storeAccountName)
-      .clickSearchForRetailerChain()
-      .selectRetailerChainFilterByName(storeAccountName)
-      .clickApplyFilters()
-      .drillIntoRightPanelWithName(storeAccountName)
-      .drillIntoRightPanelWithName(storeAccountName)
-      .clickNotesButton()
-      .waitForLoaderToDisappear();
-
-    Assert.assertTrue(notesModal.isLoaded(), "Failure to load Notes modal \n");
+    openNotesModal("Taco Joint");
   }
 
   @AfterMethod
@@ -85,6 +70,23 @@ public class NotesTest extends BaseTestCase {
     final Login loginPage = new Login(driver);
     final HomePage homePage = loginPage.loginAs(testUser);
     Assert.assertTrue(homePage.isLoaded(), "Failed to log in user: " + testUser.userName());
+  }
+
+  private void openNotesModal(String storeAccountName) {
+    final AccountDashboardPage accountDashboardPage = PageFactory.initElements(driver, AccountDashboardPage.class);
+    accountDashboardPage.goToPage();
+
+    notesModal = accountDashboardPage
+      .enterRetailerChainSearchText(storeAccountName)
+      .clickSearchForRetailerChain()
+      .selectRetailerChainFilterByName(storeAccountName)
+      .clickApplyFilters()
+      .drillIntoRightPanelWithName(storeAccountName)
+      .drillIntoRightPanelWithName(storeAccountName)
+      .clickNotesButton()
+      .waitForLoaderToDisappear();
+
+    Assert.assertTrue(notesModal.isLoaded(), "Failure to load Notes modal \n");
   }
 
 }
