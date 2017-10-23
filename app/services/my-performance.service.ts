8627d0a242eaa524090cf967a01a5b67674695bb
akshaydegaonkar <akshayprakash.degaonkar@cbrands.com>
2017-10-23 15:56:37 -0500

    US18893 - Fixed small typo

--- app/services/my-performance.service.ts
+++ app/services/my-performance.service.ts
@@ -47,12 +47,12 @@ export class MyPerformanceService {
     return defaultPremiseType;
   }
 
-  public accountDashboardStateParameters(myaccountsonly: boolean,
+  public accountDashboardStateParameters(myAccountOnly: boolean,
                                          filter: MyPerformanceFilterState,
                                         row: MyPerformanceTableRow,
                                         premiseType?: PremiseTypeValue): AccountDashboardStateParameters {
 
-    let accountDashboardStateParams: AccountDashboardStateParameters = {myaccountsonly: myaccountsonly};
+    let accountDashboardStateParams: AccountDashboardStateParameters = {myaccountsonly: myAccountOnly};
     if (row.metadata.entityType === EntityType.Distributor) {
       accountDashboardStateParams.distributorname = row.descriptionRow0;
       accountDashboardStateParams.distributorid = row.metadata.positionId;
