17bae801ca5e20be07a92df6c771b6669cbfb947
Cody Knauer <codyknauer@gmail.com>
2017-10-30 09:12:18 -0500

    US18641 - update fetch product-metrics payload w/ alt hier flag

--- app/state/actions/product-metrics.action.ts
+++ app/state/actions/product-metrics.action.ts
@@ -12,6 +12,7 @@ export interface FetchProductMetricsPayload {
   filter: MyPerformanceFilterState;
   selectedEntityType: EntityType;
   selectedBrandCode?: string;
+  alternateHierarchy?: boolean;
 }
 
 export interface FetchProductMetricsSuccessPayload {
