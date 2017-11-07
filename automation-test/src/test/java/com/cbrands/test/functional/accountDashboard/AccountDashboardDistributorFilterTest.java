c39bd2247292d49fec45c8a34ce9727eb9a79931
Edie Liao <ediel@slalom.com>
2017-11-07 13:35:39 -0600

    US19224-Adding test class for Distributor filter. Adding to functional suite.

---
+++ automation-test/src/test/java/com/cbrands/test/functional/accountDashboard/AccountDashboardDistributorFilterTest.java
@@ -0,0 +1,43 @@
+package com.cbrands.test.functional.accountDashboard;
+
+import com.cbrands.TestUser;
+import com.cbrands.pages.AccountDashboardPage;
+import com.cbrands.pages.HomePage;
+import com.cbrands.pages.Login;
+import com.cbrands.pages.LogoutPage;
+import com.cbrands.test.BaseTestCase;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class AccountDashboardDistributorFilterTest extends BaseTestCase {
+  private static TestUser testUser;
+  private LogoutPage logoutPage;
+  private AccountDashboardPage accountDashboardPage;
+
+  @BeforeMethod
+  public void setUp() {
+    testUser = TestUser.ACTOR4;
+    logoutPage = new LogoutPage(driver);
+
+    log.info("\nLoading webpage...");
+    driver.get(webAppBaseUrl);
+
+    final Login loginPage = new Login(driver);
+    final HomePage homePage = loginPage.loginAs(testUser);
+    Assert.assertTrue(homePage.isLoaded(), "Failed to log in user: " + testUser.userName());
+
+    accountDashboardPage = PageFactory.initElements(driver, AccountDashboardPage.class);
+    accountDashboardPage.goToPage();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    logoutPage.goToPage();
+  }
+
+
+}
