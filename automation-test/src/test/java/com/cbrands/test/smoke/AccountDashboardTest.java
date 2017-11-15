425e6c294c762a36fca0fc2837181e2674a9c00d
Edie Liao <ediel@slalom.com>
2017-11-14 18:01:49 -0600

    US18305-Pushed down class setup methods to individual test cases in the Smoke suite. Passed in each test name.

--- automation-test/src/test/java/com/cbrands/test/smoke/AccountDashboardTest.java
+++ automation-test/src/test/java/com/cbrands/test/smoke/AccountDashboardTest.java
@@ -10,14 +10,24 @@ import com.cbrands.pages.LogoutPage;
 import com.cbrands.test.BaseTestCase;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
+import java.net.MalformedURLException;
 
 public class AccountDashboardTest extends BaseTestCase {
   private Login loginPage;
   private LogoutPage logoutPage;
   private AccountDashboardPage accountDashboardPage;
+
+  @BeforeClass
+  public void setUpClass() throws MalformedURLException {
+    this.startUpBrowser("Smoke - AccountDashboard Test");
+  }
+
+  @AfterClass
+  public void tearDownClass() {
+    this.shutDownBrowser();
+  }
 
   @BeforeMethod
   public void setUp() {
