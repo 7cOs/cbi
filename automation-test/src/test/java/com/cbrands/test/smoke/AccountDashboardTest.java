8c38ca9ea9903fd3ffb2c31c5262fdbda502134a
Edie Liao <ediel@slalom.com>
2017-11-15 15:54:23 -0600

    Chore-ATFix-Smoke-AccountDashboard-drillupDownBrands-Formatting

--- automation-test/src/test/java/com/cbrands/test/smoke/AccountDashboardTest.java
+++ automation-test/src/test/java/com/cbrands/test/smoke/AccountDashboardTest.java
@@ -49,10 +49,14 @@ public class AccountDashboardTest extends BaseTestCase {
       .waitForBrandsPanelLoaderToDisappear()
       .waitForMarketPanelLoaderToDisappear();
 
-    Assert.assertTrue(accountDashboardPage.isLeftPanelResultsLoadedFor(LeftPanelLevel.Brand), "Left brands panel failed to load " +
-      "results");
-    Assert.assertTrue(accountDashboardPage.isRightPanelResultsLoadedFor(RightPanelLevel.Accounts), "Right accounts panel failed to " +
-      "load results");
+    Assert.assertTrue(
+      accountDashboardPage.isLeftPanelResultsLoadedFor(LeftPanelLevel.Brand),
+      "Left brands panel failed to load results"
+    );
+    Assert.assertTrue(
+      accountDashboardPage.isRightPanelResultsLoadedFor(RightPanelLevel.Accounts),
+      "Right accounts panel failed to load results"
+    );
   }
 
   @Test(description = "Drill all the way down the account hierarchy and drill back up")
@@ -126,7 +130,7 @@ public class AccountDashboardTest extends BaseTestCase {
   }
 
   @Test(description = "Drill all the way down the brands hierarchy and drill back up")
-  public void drillDownDrillUpBrands () {
+  public void drillDownDrillUpBrands() {
     accountDashboardPage.drillIntoFirstRowInLeftPanel();
     Assert.assertTrue(
       accountDashboardPage.isLeftPanelResultsLoadedFor(LeftPanelLevel.SkuPackage),
