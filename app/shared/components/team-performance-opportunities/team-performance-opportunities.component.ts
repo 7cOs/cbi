afbe1ef409b5fcc1e655a2e2f60e58442d855802
Piotr Bogun <pbogun1123@gmail.com>
2017-11-20 17:37:32 -0600

    US175578 - adds further component buildout and styling

--- app/shared/components/team-performance-opportunities/team-performance-opportunities.component.ts
+++ app/shared/components/team-performance-opportunities/team-performance-opportunities.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'team-performance-opportunities',
@@ -7,5 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 
 export class TeamPerformanceOpportunitiesComponent {
-  @Input() header: string;
+  @Output() onCloseIndicatorClicked = new EventEmitter<any>();
+
+  @Input() subTitle: string;
 }
