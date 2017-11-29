4cd855a065da1eba62cfd6ce688075a2b06a63ef
Edie Liao <ediel@slalom.com>
2017-11-29 15:29:26 -0600

    DE6349-Inlining smoke login test code for conciseness.

--- automation-test/src/test/java/com/cbrands/test/smoke/LoginTest.java
+++ automation-test/src/test/java/com/cbrands/test/smoke/LoginTest.java
@@ -5,7 +5,6 @@ import com.cbrands.pages.LoginPage;
 import com.cbrands.pages.LogoutPage;
 import com.cbrands.test.BaseTestCase;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -15,33 +14,22 @@ import java.net.MalformedURLException;
  * Automated test for logging in and out of the web app.
  */
 public class LoginTest extends BaseTestCase {
-  private LoginPage loginPage;
-  private LogoutPage logoutPage;
 
-  @AfterMethod
-  public void tearDown() {
-    logoutPage.goToPage();
+  @Test(dataProvider = "userCredentials", description = "Testing basic login and logout")
+  public void testLogin(TestUser testUser) throws MalformedURLException {
+    this.startUpBrowser("Smoke - LoginTest: " + testUser.fullName());
+
+    PageFactory.initElements(driver, LoginPage.class).loginAs(testUser);
+    PageFactory.initElements(driver, LogoutPage.class).goToPage();
 
     this.shutDownBrowser();
   }
 
-  @Test(dataProvider = "userCredentials", description = "Testing basic login and logout")
-  public void testLogin(TestUser testUser) throws MalformedURLException {
-    this.setUpTestWith(testUser);
-    loginPage.loginAs(testUser);
-  }
-
-  private void setUpTestWith(TestUser testUser) throws MalformedURLException {
-    this.startUpBrowser("Smoke - LoginTest: " + testUser.fullName());
-    loginPage = PageFactory.initElements(driver, LoginPage.class);
-    logoutPage = PageFactory.initElements(driver, LogoutPage.class);
-  }
-
   @DataProvider(name = "userCredentials")
   public static Object[][] userCredentials() {
-    return new Object[][] {
-      { TestUser.ACTOR4 } ,
-      { TestUser.NOTES_ACTOR }
+    return new Object[][]{
+      {TestUser.ACTOR4},
+      {TestUser.NOTES_ACTOR}
     };
   }
 
