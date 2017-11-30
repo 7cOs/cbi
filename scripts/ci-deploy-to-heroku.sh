46a03015230690e53dc2aaa5bf389f02a5634add
Matt Ritter <matt.ritter@slalom.com>
2017-11-30 17:05:46 -0600

    US19958 - Add test suite variable as input param to AT script; Move mvn install dependency to AT script so this step only happens when needed.

--- scripts/ci-deploy-to-heroku.sh
+++ scripts/ci-deploy-to-heroku.sh
@@ -3,6 +3,8 @@
 GIT_ENVIRONMENT=$1
 
 if [ ! $skipDeployment ] || ( [ $skipDeployment ] && ! $skipDeployment ) ; then
-  echo $CIRCLE_SHA1
+  echo Pushing $CIRCLE_SHA1 to $GIT_ENVIRONMENT
   git push git@heroku.com:$GIT_ENVIRONMENT $CIRCLE_SHA1:master -f
+else
+  echo Skipping Deployment to Heroku
 fi
--- scripts/ci-run-automation-tests.sh
+++ scripts/ci-run-automation-tests.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+TEST_SUITE=$1
+
 cd automation-test
+mvn install -DskipTests=true
 
 if [ $runTestSuite ] ; then
-  mvn test -P $runTestSuite -DargLine="-Denv=qa -Dbrowser=ie"
-  mvn test -P $runTestSuite -DargLine="-Denv=qa -Dbrowser=chrome"
+  echo Running specified test suite $TEST_SUITE
+  mvn test -P $TEST_SUITE -DargLine="-Denv=qa -Dbrowser=ie"
+  mvn test -P $TEST_SUITE -DargLine="-Denv=qa -Dbrowser=chrome"
 else
+  echo Running default test suite
   mvn test -DargLine="-Denv=qa -Dbrowser=ie"
   mvn test -DargLine="-Denv=qa -Dbrowser=chrome"
 fi
