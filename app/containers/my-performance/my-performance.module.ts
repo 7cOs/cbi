bcb21eeb9782a2a1e41a6fa406b99e5d6f8d8702
Piotr Bogun <pbogun1123@gmail.com>
2017-11-30 16:28:24 -0600

    US17578 - Adds minor cleanup

--- app/containers/my-performance/my-performance.module.ts
+++ app/containers/my-performance/my-performance.module.ts
@@ -19,8 +19,8 @@ import { ResponsibilitiesTransformerService } from  '../../services/responsibili
 import { ResponsibilitiesService } from  '../../services/responsibilities.service';
 import { SharedModule } from '../../shared/shared.module';
 import { SortIndicatorComponent } from '../../shared/components/sort-indicator/sort-indicator.component';
-import { TeamPerformanceOpportunitiesComponent
-} from '../../shared/components/team-performance-opportunities/team-performance-opportunities.component';
+import { TeamPerformanceOpportunitiesComponent }
+  from '../../shared/components/team-performance-opportunities/team-performance-opportunities.component';
 import { WindowService } from '../../services/window.service';
 
 @NgModule({
