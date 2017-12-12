fdf186cdefdb60ef80f76935d744fd1ccd46461b
Corentin Leman <corentin.leman@gmail.com>
2017-12-12 11:01:44 -0600

    US20178 - Added slack app in router.

---
+++ server/routes/slackApp.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const rp = require('request-promise');
+
+const circleCIClient = require('../../lib/circle-ci-client');
+const parametersUtils = require('../../lib/trigger-ci-parameters-utils');
+
+module.exports = function(app) {
+  app.post('/slack-app', (req, res) => {
+    console.log(req.body);
+
+// check token
+
+    const parameters = parametersUtils.parse(req.body.text.split(' '));
+
+    circleCIClient.client.buildBranch(parameters)
+    .then((response) => {
+      console.log('then called');
+      // console.log(response);
+      sendUpdateToSlashFunction(req.body.response_url, 'Build created!');
+    })
+    .catch((error) => {
+      console.log('error called');
+      console.log(error);
+      sendUpdateToSlashFunction(req.body.response_url, 'There was an error creating the build');
+    });
+
+    res.json({'text': 'Requesting build...'});
+  });
+};
+
+function sendUpdateToSlashFunction(url, text) {
+  console.log('Sending updates');
+  console.log(`url = ${url}`);
+  const options = {
+    method: 'POST',
+    uri: url,
+    body: {
+      'text': text
+    },
+    json: true
+  };
+
+  rp(options)
+  .then((parsedBody) => {
+    console.log('Update sent!');
+    // console.log(parsedBody);
+  })
+  .catch((error) => {
+    console.log('error sending an update');
+    console.log(error);
+  });
+}
