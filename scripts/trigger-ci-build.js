8a3295b97e78f59f165ee5a8e4110be1bf717839
Corentin Leman <corentin.leman@gmail.com>
2017-12-12 14:44:16 -0600

    US20178 - Refactor build instructions inside parameters util.

--- scripts/trigger-ci-build.js
+++ scripts/trigger-ci-build.js
@@ -1,27 +1,27 @@
 const circleCIClient = require('../lib/circle-ci-client');
 const parametersUtils = require('../lib/trigger-ci-parameters-utils');
 
-const parameters = parametersUtils.parse(process.argv);
+try {
+  const parameters = parametersUtils.parse(process.argv);
+  triggerBuild(parameters);
+} catch (error) {
+  console.log(error.message);
+  console.log(parametersUtils.instructions);
+}
 
-if (parameters.project && parameters.project.length &&
-  parameters.branchName && parameters.branchName.length &&
-  parameters.token && parameters.token.length) {
-    circleCIClient.buildBranch(parameters)
-    .then((parsedBody) => {
-      console.log('Build created with success!');
-    })
-    .catch((err) => {
-      console.log('Error when creating the build');
-      console.log(err);
-    });
-} else {
-  console.log('Please provide valid arguments. Usage:');
-  console.log('  node trigger-ci-build.js opt1:val1 opt2:val2 ...');
-  console.log('Options:');
-  console.log(`  ${parametersUtils.parametersArgNames.projectArgName}<repo/ci-project> [default: compass-portal]`);
-  console.log(`  ${parametersUtils.parametersArgNames.branchArgName}<branch> [default: test]`);
-  console.log(`  ${parametersUtils.parametersArgNames.tokenArgName}<ci-token> [default: process.env.CIRCLE_CI_API_TOKEN]`);
-  console.log(`  ${parametersUtils.parametersArgNames.runTestSuiteArgName}<suite>`);
-  console.log(`  ${parametersUtils.parametersArgNames.runTestSuiteEnvtArgName}<suite-environment> [default: qa] [possible values:[${parametersUtils.possibleEnvironments}]]`);
-  console.log(`  ${parametersUtils.parametersArgNames.skipDeploymentArgName}<true|false> [default: true]`);
+function triggerBuild(parameters) {
+  if (parameters.project && parameters.project.length &&
+    parameters.branchName && parameters.branchName.length &&
+    parameters.token && parameters.token.length) {
+      circleCIClient.buildBranch(parameters)
+      .then((parsedBody) => {
+        console.log('Build created with success!');
+      })
+      .catch((error) => {
+        console.log('Error when creating the build');
+        throw error;
+      });
+  } else {
+    throw new Error('Insufficient arguments');
+  }
 }
