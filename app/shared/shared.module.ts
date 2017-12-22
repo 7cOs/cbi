f47eea3935831218a4c7dd261f2c36374c527fdb
Matt Ritter <matt.ritter@slalom.com>
2017-12-22 17:22:57 -0600

    chore - add MatCardModule to share module imports.

--- app/shared/shared.module.ts
+++ app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule }   from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { MatRadioModule, MatRippleModule, MatSelectModule, MatSidenavModule } from '@angular/material';
+import { MatCardModule, MatRadioModule, MatRippleModule, MatSelectModule, MatSidenavModule } from '@angular/material';
 
 import { CompassCardComponent }  from './components/compass-card/compass-card.component';
 import { CompassRadioComponent } from './components/compass-radio/compass-radio.component';
@@ -14,6 +14,7 @@ import { CompassTooltipComponent } from './components/compass-tooltip/compass-to
     BrowserAnimationsModule,
     CommonModule,
     FormsModule,
+    MatCardModule,
     MatRadioModule,
     MatSelectModule
   ],
