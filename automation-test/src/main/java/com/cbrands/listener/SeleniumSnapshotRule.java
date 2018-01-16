8f61b60fd8a13a60a3798c3b7bce9d9763ad674b
Edie Liao <ediel@slalom.com>
2018-01-16 15:48:18 -0600

    DE6829 - Updating screenshot names to include a browser prefix.

--- automation-test/src/main/java/com/cbrands/listener/SeleniumSnapshotRule.java
+++ automation-test/src/main/java/com/cbrands/listener/SeleniumSnapshotRule.java
@@ -8,20 +8,22 @@ import org.testng.TestListenerAdapter;
 import com.cbrands.helper.SeleniumUtils;
 
 /**
- * 
+ *
  * @author Kazi Hossain
  *
  */
 public class SeleniumSnapshotRule extends TestListenerAdapter {
-	private Log log = LogFactory.getLog(SeleniumSnapshotRule.class);
+  private static final String SCREENSHOTS_PATH = "surefire-reports/screenshots/";
+
+  private Log log = LogFactory.getLog(SeleniumSnapshotRule.class);
 
 	@Override
-	public void onTestFailure(ITestResult tr) {
-		String basePath = "target/screenshot/";
-		String outputFileName = tr.getMethod().getMethodName() + ".png";
-		SeleniumUtils.snapshot(basePath, outputFileName);
+	public void onTestFailure(ITestResult failedTest) {
+    final String failedMethod = failedTest.getMethod().getMethodName();
+
+    SeleniumUtils.snapshot(SCREENSHOTS_PATH, getScreenshotNameFor(failedMethod));
 		log.info("*****************************************************");
-		log.info("TEST FAILED: " + tr.getMethod().getMethodName() + "!!");
+		log.info("TEST FAILED: " + failedMethod + "!!");
 		log.info("*****************************************************");
 	}
 
@@ -29,4 +31,14 @@ public class SeleniumSnapshotRule extends TestListenerAdapter {
 	public void onTestSuccess(ITestResult tr) {
 		log.info("TEST PASSED: " + tr.getMethod().getMethodName());
 	}
+
+  private String getScreenshotNameFor(String failedMethodName) {
+    final String browserPrefix = getBrowserPrefix();
+    return browserPrefix + failedMethodName + ".png";
+  }
+
+  private String getBrowserPrefix() {
+    final String browser = System.getProperty("browser");
+    return browser != null ? browser + "_" : "";
+  }
 }
