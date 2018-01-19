44a98ffdc92a52ea7fae0686ad5a0abab78c0e3f
Cody Knauer <codyknauer@gmail.com>
2018-01-19 08:52:25 -0600

    US19843 - specs for analytics tracker

--- app/containers/my-performance/my-performance.component.spec.ts
+++ app/containers/my-performance/my-performance.component.spec.ts
@@ -3855,6 +3855,27 @@ describe('MyPerformanceComponent', () => {
         }
       );
     });
+
+    it('should call analyticsService#track event with subAccountID for label when a subAccount is selected', () => {
+      componentInstanceCopy.selectedSubaccountCode = chance.string();
+      componentInstance.handleOpportunityClicked(opportunity);
+      expect(analyticsServiceMock.trackEvent).toHaveBeenCalledWith(
+        'Navigation',
+        'Go To Opportunities',
+        componentInstanceCopy.selectedSubaccountCode
+      );
+    });
+
+    it('should call analyticsService#track event with distributorCode for label when a subAccount is NOT selected', () => {
+      componentInstanceCopy.selectedDistributorCode = chance.string();
+      componentInstanceCopy.selectedSubaccountCode = null;
+      componentInstance.handleOpportunityClicked(opportunity);
+      expect(analyticsServiceMock.trackEvent).toHaveBeenCalledWith(
+        'Navigation',
+        'Go To Opportunities',
+        componentInstanceCopy.selectedDistributorCode
+      );
+    });
   });
 
   describe('handleOpportunityCountTotalClicked', () => {
