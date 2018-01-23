97323a78359bdbe1b57e7876fe33fb830010d1cb
Michael Oguejiofor <chinedu@gmail.com>
2018-01-23 16:29:36 -0600

    US21216 - added paragraph capabilities to tooltip

--- app/shared/components/compass-tooltip/compass-tooltip.component.spec.ts
+++ app/shared/components/compass-tooltip/compass-tooltip.component.spec.ts
@@ -7,8 +7,8 @@ describe('CompassTooltipComponent', () => {
   let fixture: ComponentFixture<CompassTooltipComponent>;
   let componentInstance: CompassTooltipComponent;
 
+  let descriptionMock: Array <string>;
   let titleMock: string;
-  let descriptionMock: string;
   let positionMock: string;
   let labelMock: string;
 
@@ -20,26 +20,26 @@ describe('CompassTooltipComponent', () => {
     fixture = TestBed.createComponent(CompassTooltipComponent);
     componentInstance = fixture.componentInstance;
 
+    descriptionMock = [ chance.string() ];
     titleMock = chance.string();
-    descriptionMock = chance.string();
     positionMock = chance.string();
     labelMock = chance.string();
   });
 
   describe('TooltipComponent with title', () => {
     it('should contain tooltip with header title and position = title_below', () => {
-      componentInstance.description = descriptionMock;
+      componentInstance.descriptions = descriptionMock;
       componentInstance.title = titleMock;
       fixture.detectChanges();
       const elementText = fixture.debugElement.query(By.css('.tooltip_text_with_title'));
       const elementPosition = fixture.debugElement.query(By.css('.title_below'));
       expect(elementText).not.toBe(null);
       expect(elementPosition).not.toBe(null);
-      expect(elementText.nativeElement.textContent).toBe(componentInstance.title + componentInstance.description);
+      expect(elementText.nativeElement.textContent).toBe(componentInstance.title + componentInstance.descriptions[0]);
     });
 
     it('should select tooltip with header title, position title_above', () => {
-      componentInstance.description = descriptionMock;
+      componentInstance.descriptions = descriptionMock;
       componentInstance.title = titleMock;
       componentInstance.position = 'above';
       fixture.detectChanges();
@@ -47,11 +47,11 @@ describe('CompassTooltipComponent', () => {
       const elementPosition = fixture.debugElement.query(By.css('.title_above'));
       expect(elementText).not.toBe(null);
       expect(elementPosition).not.toBe(null);
-      expect(elementText.nativeElement.textContent).toBe(componentInstance.title + componentInstance.description);
+      expect(elementText.nativeElement.textContent).toBe(componentInstance.title + componentInstance.descriptions[0]);
     });
 
     it('should select tooltip with header title, position title_right', () => {
-      componentInstance.description = descriptionMock;
+      componentInstance.descriptions = descriptionMock;
       componentInstance.title = titleMock;
       componentInstance.position = 'right';
       fixture.detectChanges();
@@ -59,11 +59,11 @@ describe('CompassTooltipComponent', () => {
       const elementPosition = fixture.debugElement.query(By.css('.title_right'));
       expect(elementText).not.toBe(null);
       expect(elementPosition).not.toBe(null);
-      expect(elementText.nativeElement.textContent).toBe(componentInstance.title + componentInstance.description);
+      expect(elementText.nativeElement.textContent).toBe(componentInstance.title + componentInstance.descriptions[0]);
     });
 
     it('should select tooltip with header title, position title_left', () => {
-      componentInstance.description = descriptionMock;
+      componentInstance.descriptions = descriptionMock;
       componentInstance.title = titleMock;
       componentInstance.position = 'left';
       fixture.detectChanges();
@@ -71,54 +71,54 @@ describe('CompassTooltipComponent', () => {
       const elementPosition = fixture.debugElement.query(By.css('.title_left'));
       expect(elementText).not.toBe(null);
       expect(elementPosition).not.toBe(null);
-      expect(elementText.nativeElement.textContent).toBe(componentInstance.title + componentInstance.description);
+      expect(elementText.nativeElement.textContent).toBe(componentInstance.title + componentInstance.descriptions[0]);
     });
 
   });
 
   describe('TooltipComponent without title', () => {
     it('should select tooltip with no title, position no_title_below', () => {
-      componentInstance.description = descriptionMock;
+      componentInstance.descriptions = descriptionMock;
       componentInstance.position = 'below';
       fixture.detectChanges();
       const elementText = fixture.debugElement.query(By.css('.tooltip_text_no_title'));
       const elementPosition = fixture.debugElement.query(By.css('.no_title_below'));
       expect(elementText).not.toBe(null);
       expect(elementPosition).not.toBe(null);
-      expect(elementText.nativeElement.textContent).toBe(componentInstance.description);
+      expect(elementText.nativeElement.textContent).toBe(componentInstance.descriptions[0]);
     });
 
     it('should select tooltip without header title, position no_title_above', () => {
-      componentInstance.description = descriptionMock;
+      componentInstance.descriptions = descriptionMock;
       componentInstance.position = 'above';
       fixture.detectChanges();
       const elementText = fixture.debugElement.query(By.css('.tooltip_text_no_title'));
       const elementPosition = fixture.debugElement.query(By.css('.no_title_above'));
       expect(elementText).not.toBe(null);
       expect(elementPosition).not.toBe(null);
-      expect(elementText.nativeElement.textContent).toBe(componentInstance.description);
+      expect(elementText.nativeElement.textContent).toBe(componentInstance.descriptions[0]);
     });
 
     it('should select tooltip without header title, position no_title_right', () => {
-      componentInstance.description = descriptionMock;
+      componentInstance.descriptions = descriptionMock;
       componentInstance.position = 'right';
       fixture.detectChanges();
       const elementText = fixture.debugElement.query(By.css('.tooltip_text_no_title'));
       const elementPosition = fixture.debugElement.query(By.css('.no_title_right'));
       expect(elementText).not.toBe(null);
       expect(elementPosition).not.toBe(null);
-      expect(elementText.nativeElement.textContent).toBe(componentInstance.description);
+      expect(elementText.nativeElement.textContent).toBe(componentInstance.descriptions[0]);
     });
 
     it('should select tooltip without header title, position no_title_left', () => {
-      componentInstance.description = descriptionMock;
+      componentInstance.descriptions = descriptionMock;
       componentInstance.position = 'left';
       fixture.detectChanges();
       const elementText = fixture.debugElement.query(By.css('.tooltip_text_no_title'));
       const elementPosition = fixture.debugElement.query(By.css('.no_title_left'));
       expect(elementText).not.toBe(null);
       expect(elementPosition).not.toBe(null);
-      expect(elementText.nativeElement.textContent).toBe(componentInstance.description);
+      expect(elementText.nativeElement.textContent).toBe(componentInstance.descriptions[0]);
     });
   });
 
