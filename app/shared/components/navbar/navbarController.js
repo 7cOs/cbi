2a0c459fb13b42e339c60600ab86196d89d4a79a
Jeremy Kalinowski <jeremy.kalinowski@cbrands.com>
2018-02-02 14:05:54 -0500

    US21660 - Modified the tests to move the opportunity up for access across. Made modifications to reuse the sendAddOpportunityAnalyticsEvent.

--- app/shared/components/navbar/navbarController.js
+++ app/shared/components/navbar/navbarController.js
@@ -351,7 +351,7 @@ module.exports = /*  @ngInject */
           vm.newOpportunity = vm.newOpportunityTemplate;
           resetFormModels();
           if (success.id) {
-            sendAddOpportunityAnalyticsEvent(success.id);
+            sendAddOpportunityAnalyticsEvent(success.id, null);
           }
         }, function(error) {
           console.log(error);
@@ -365,23 +365,27 @@ module.exports = /*  @ngInject */
       return true;
     }
 
-    function sendAddOpportunityAnalyticsEvent(opportunityId) {
-      analyticsService.trackEvent(
-        'Opportunities',
-        'Add Opportunity',
-        opportunityId
-      );
+    function sendAddOpportunityAnalyticsEvent(opportunityId, targetListId) {
+      if (opportunityId) {
+        analyticsService.trackEvent(
+          'Opportunities',
+          'Add Opportunity',
+          opportunityId
+        );
+      } else if (targetListId) {
+        analyticsService.trackEvent(
+          'Opportunities',
+          'Add To Target List',
+          targetListId
+        );
+      }
     }
 
     function addToTargetList(targetList, opportunity) {
       var storeExists = false;
 
       targetListService.addTargetListOpportunities(targetList, [opportunity.id]).then(function(success) {
-        analyticsService.trackEvent(
-          'Opportunities',
-          'Add To Target List',
-          targetList
-        );
+        sendAddOpportunityAnalyticsEvent(null, targetList);
       });
 
       opportunity.brands = [];
