4b38314a632e72a38372dc2e5e6cf3da7994ee6a
SKARNEH <SKARNEH@PC0QMY9T.cwc.cbi.net>
2018-03-08 14:41:46 -0600

    Removed detailed code comments as post PR feedback

--- automation-test/src/test/java/com/cbrands/test/functional/opportunities/filters/OpportunitiesResetFiltersTest.java
+++ automation-test/src/test/java/com/cbrands/test/functional/opportunities/filters/OpportunitiesResetFiltersTest.java
@@ -1,31 +1,15 @@
 package com.cbrands.test.functional.opportunities.filters;
 
-import com.cbrands.PremiseType;
+
 import com.cbrands.TestUser;
 import com.cbrands.pages.LoginPage;
 import com.cbrands.pages.LogoutPage;
 import com.cbrands.pages.opportunities.OpportunitiesPage;
 import com.cbrands.test.BaseTestCase;
-import com.google.gson.JsonObject;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Properties;
 
 public class OpportunitiesResetFiltersTest extends BaseTestCase {
 
@@ -48,27 +32,21 @@ public class OpportunitiesResetFiltersTest extends BaseTestCase {
   }
 
   
-  @Test(
-	  description = "US23292 - Reset filters before clicking Applying Filters button", 
-	  dataProvider = "US23292Data", priority = 1, invocationCount = 1 )
+  @Test( description = "Reset filters before clicking Applying Filters button", 
+	  dataProvider = "resetFiltersData", priority = 1, invocationCount = 1 )
   public void resetBeforeApplyingFilters( TestUser usr, String dist ) {
 	  
-	  // - Log in to AUT; navigate to Opps page/component - //
 	 loginToOpportunitiesPage( usr );
 
-	 // - Enter, search, select distributor - //
 	 selectDistributor( oppsPg, dist );
 
-	 // - Confirm selected distributor chip label is displayed above 'Active Filters' form - //
 	 Assert.assertTrue( oppsPg.isQueryChipPresent( dist ), 
-			  "Selected distributor chip IS NOT present post distributer select" );
+			  "Selected distributor chip IS NOT present post distributor select" );
 	  
-	  // - Locate/click 'Reset' link - //
 	 oppsPg.clickResetFilters();
 	  
-	  // - Confirm selected distributor chip label is displayed above 'Active Filters' form - //
 	  Assert.assertFalse( oppsPg.isQueryChipPresent( dist ), 
-				  "Selected distributor chip IS present post distributer select" );	  
+				  "Selected distributor chip IS present post distributor select" );	  
   }
 
 
@@ -79,54 +57,12 @@ public class OpportunitiesResetFiltersTest extends BaseTestCase {
   }
   
   
-  public static void selectDistributor( String dist ) {
-	  
-	  // - Enter, search, and select name in Distributor field- //
-	  WebElement f  = q ( "//*[@placeholder='Name']//input[@ng-model='is.input']" );
-	  f.sendKeys(  dist );
-	  // - Locate search button - //
-	  WebElement b = q( "//input[@class='submit-btn visible']" );
-	  b.click();
-	  // - Select distributor - search results - //
-	  List<WebElement> rs = qs( "//*[@class='results-container open']//ul//li" );
-	  rs.get(0).click();
-	  
-  }
-  
-  
-  public static WebElement q ( String xp ) {
-	  if( qs ( xp ).size() > 0 ) { 
-		  return (WebElement)qs( xp).get( 0 ); 
-	  }
-	  return null;
-  }
-  
-  
-  public static List<WebElement> qs ( String xp ) {
-	  return driver.findElements( By.xpath( xp ) );
-  }
-  
-  
-  public static JavascriptExecutor jse() {
-	  return (JavascriptExecutor) driver;
-  }
-  
-  
-  
   @DataProvider
-  public static Object[][] US23292Data() {
+  public static Object[][] resetFiltersData() {
     return new Object[][]{
       {TestUser.ACTOR4, "Healy Wholesale"}
     };
   }
-  
-  @DataProvider
-  public static Object[][] US23293Data() {
-    return new Object[][]{
-      {TestUser.ACTOR4, "Chicago Bev Systems - Il"}
-    };
-  }
-
   
   private void loginToOpportunitiesPage(TestUser user) {
     PageFactory.initElements(driver, LoginPage.class).loginAs(user);
