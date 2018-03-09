175b70f8d4b3e20b1027303c82b2b06d952428f1
SKARNEH <SKARNEH@PC0QMY9T.cwc.cbi.net>
2018-03-09 15:46:24 -0600

    IDE configured to indent code 2 spaces

--- automation-test/src/test/java/com/cbrands/test/functional/opportunities/filters/OpportunitiesResetFiltersTest.java
+++ automation-test/src/test/java/com/cbrands/test/functional/opportunities/filters/OpportunitiesResetFiltersTest.java
@@ -13,13 +13,11 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-
 public class OpportunitiesResetFiltersTest extends BaseTestCase {
 
   private OpportunitiesPage opportunitiesPage;
   private String distributor;
 
- 
   @AfterClass
   public void tearDownClass() {
     this.shutDownBrowser();
@@ -27,38 +25,34 @@ public class OpportunitiesResetFiltersTest extends BaseTestCase {
 
   @BeforeMethod
   public void setUp(Method method) throws MalformedURLException {
-	
-	distributor = "Healy Wholesale"; 
-	  
-    this.startUpBrowser(String.format("Functional - Opportunities Reset Filter Test - %s", 
-    		method.getAnnotation(Test.class).description()));
-    
+
+    distributor = "Healy Wholesale";
+
+    this.startUpBrowser(String.format("Functional - Opportunities Reset Filter Test - %s",
+        method.getAnnotation(Test.class).description()));
+
     PageFactory.initElements(driver, LoginPage.class).loginAs(TestUser.ACTOR4);
     opportunitiesPage = PageFactory.initElements(driver, OpportunitiesPage.class);
     opportunitiesPage.goToPage();
   }
-  
-  
+
   @AfterMethod
   public void tearDown() {
     PageFactory.initElements(driver, LogoutPage.class).goToPage();
   }
 
-  
-  @Test( description = "Reset filters before clicking Applying Filters button" )
+  @Test(description = "Reset filters before clicking Applying Filters button")
   public void resetBeforeApplyingFilters() {
-	  
-	  opportunitiesPage.enterDistributorSearchText( distributor )
-  		.clickSearchForDistributor()
-  		.clickFirstDistributorResult();
 
-	 Assert.assertTrue( opportunitiesPage.isQueryChipPresent( distributor ), 
-			  "Selected distributor chip IS NOT present post distributor select" );
-	  
-	 opportunitiesPage.clickResetFilters();
-	  
-	  Assert.assertFalse( opportunitiesPage.isQueryChipPresent( distributor ), 
-				  "Selected distributor chip IS present post distributor select" );	  
+    opportunitiesPage.enterDistributorSearchText(distributor).clickSearchForDistributor().clickFirstDistributorResult();
+
+    Assert.assertTrue(opportunitiesPage.isQueryChipPresent(distributor),
+        "Selected distributor chip IS NOT present post distributor select");
+
+    opportunitiesPage.clickResetFilters();
+
+    Assert.assertFalse(opportunitiesPage.isQueryChipPresent(distributor),
+        "Selected distributor chip IS present post distributor select");
   }
-  
+
 }
