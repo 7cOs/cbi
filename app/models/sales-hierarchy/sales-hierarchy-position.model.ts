0999394fdd3bdafb72203a9cb43c24944f44bd3c
Jeremy Kalinowski <jeremy.kalinowski@cbrands.com>
2018-03-14 11:11:32 -0400

    US22181 - Added SalesHierarchyTypes and made the position and positiondescriptions better.

--- app/models/sales-hierarchy/sales-hierarchy-position.model.ts
+++ app/models/sales-hierarchy/sales-hierarchy-position.model.ts
@@ -5,6 +5,6 @@ export interface SalesHierarchyPosition extends SalesHierarchyEntity {
   salesHierarchyType: SalesHierarchyType;
   employeeId: string;
   isOpenPosition?: boolean;
-  positionDescription?: string; // Title of the position
-  description?: string; // Role group they belong to
+  positionLocation?: string; // Title of the position
+  positionRoleGroup?: string;
 }
