7289559b50ba79fdb9005597ce89a97f0e24925f
Edie Liao <ediel@slalom.com>
2018-03-29 16:52:42 -0500

    CI6925-Removing unused imports

--- automation-test/src/test/java/com/cbrands/pages/lists/ListsPage.java
+++ automation-test/src/test/java/com/cbrands/pages/lists/ListsPage.java
@@ -9,10 +9,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.FluentWait;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static com.cbrands.helper.SeleniumUtils.*;
 
