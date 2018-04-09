fbc010d6cb3541231ac878bdee6ccb246fc0f1f2
akshaydegaonkar <akshayprakash.degaonkar@cbrands.com>
2018-04-09 10:55:25 -0500

    US23614 - created skeleton for folder

--- app/containers/lists/list-detail.module.ts
+++ app/containers/lists/list-detail.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { ListDetailComponent } from '../../containers/lists/list-detail.component';
+import { ListsHeaderComponent } from '../../shared/components/lists-header/lists-header.component';
+import { SharedModule } from '../../shared/shared.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    CommonModule,
+    SharedModule
+  ],
   declarations: [
-    ListDetailComponent
+    ListDetailComponent,
+    ListsHeaderComponent
   ]
 })
 
---
+++ app/shared/components/lists-header/lists-header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lists-header',
+  template: require('./lists-header.pug'),
+  styles: [ require('./lists-header.scss') ]
+})
+export class ListsHeaderComponent {
+}
