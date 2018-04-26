c277c3f863f7d8c777bb51f536f86ebe44ef0481
Cody Knauer <codyknauer@gmail.com>
2018-04-26 09:14:06 -0500

    US23707 - utilize v3 endpoints and transfomer on landing page

--- app/shared/components/target/targetListController.js
+++ app/shared/components/target/targetListController.js
@@ -94,24 +94,24 @@ module.exports = /*  @ngInject */
     // PRIVATE METHODS
     // **************
 
-    function curriedFilterByArchived(yes) {
-      return function(item) {
-        /* eslint eqeqeq: 0 */
-        /* needed this so as to allow coercion */
-        return item.archived == yes;
-      };
-    }
+    // function curriedFilterByArchived(yes) {
+    //   return function(item) {
+    //     /* eslint eqeqeq: 0 */
+    //     /* needed this so as to allow coercion */
+    //     return item.archived == yes;
+    //   };
+    // }
 
-    // Filter archived or deleted target lists
-    function filterTargetLists(list) {
-      var filteredList = [];
-      angular.forEach(list, function(value, key) {
-        if (!(value.deleted || value.archived)) {
-          filteredList.push(value);
-        }
-      });
-      return filteredList;
-    }
+    // // Filter archived or deleted target lists
+    // function filterTargetLists(list) {
+    //   var filteredList = [];
+    //   angular.forEach(list, function(value, key) {
+    //     if (!(value.deleted || value.archived)) {
+    //       filteredList.push(value);
+    //     }
+    //   });
+    //   return filteredList;
+    // }
 
     function init() {
       if (userService.model.targetLists) {
@@ -119,34 +119,43 @@ module.exports = /*  @ngInject */
       }
 
       loaderService.openLoader();
-      userService.getTargetLists(userService.model.currentUser.employeeID).then(function(data) {
+      const currentUserEmployeeID = userService.model.currentUser.employeeID;
+      listsApiService.getLists().toPromise().then((response) => {
+        let lists = userService.model.targetLists = listsTransformerService.getV2ListsSummary(response, currentUserEmployeeID);
+        userService.model.targetLists = lists;
+
+        vm.types.mine.records = lists.ownedNotArchivedTargetLists;
+        vm.types.mine.total = lists.ownedNotArchived;
+        vm.types.shared.records = lists.sharedNotArchived;
+        vm.types.shared.total = lists.sharedNotArchivedCount;
         loaderService.closeLoader();
-        userService.model.targetLists = data;
-        // split things into categories, but ignore archived
-        var mine = data.owned.filter(curriedFilterByArchived(false));
-        if (data.sharedWithMe) var shared = data.sharedWithMe.filter(curriedFilterByArchived(false));
-
-        // Get archived
-        var archived = data.owned.filter(curriedFilterByArchived(true));
-
-        // Uncomment this if we want shared archived included with my archived
-        // archived = archived.concat(
-        //   data.sharedWithMe.filter(curriedFilterByArchived(true))
-        // );
-
-        // Send to model
-        vm.types.mine.records = filterTargetLists(mine);
-        vm.types.mine.total = mine.length;
-        userService.model.targetLists.owned = mine;
-
-        vm.types.shared.records = filterTargetLists(shared);
-        vm.types.shared.total = shared.length;
-        vm.types.archived.records = filterTargetLists(archived);
-        vm.types.archived.total = archived.length;
-      }, function(reason) {
-        console.log('Error: ' + reason);
+      }, (error) => {
+        console.log('Error: ' + error);
         loaderService.closeLoader();
       });
+      // userService.getTargetLists(userService.model.currentUser.employeeID).then(function(data) {
+      //   loaderService.closeLoader();
+      //   userService.model.targetLists = data;
+      //   // split things into categories, but ignore archived
+      //   var mine = data.owned.filter(curriedFilterByArchived(false));
+      //   if (data.sharedWithMe) var shared = data.sharedWithMe.filter(curriedFilterByArchived(false));
+
+      //   // Get archived
+      //   var archived = data.owned.filter(curriedFilterByArchived(true));
+
+      //   // Send to model
+      //   vm.types.mine.records = filterTargetLists(mine);
+      //   vm.types.mine.total = mine.length;
+      //   userService.model.targetLists.owned = mine;
+
+      //   vm.types.shared.records = filterTargetLists(shared);
+      //   vm.types.shared.total = shared.length;
+      //   vm.types.archived.records = filterTargetLists(archived);
+      //   vm.types.archived.total = archived.length;
+      // }, function(reason) {
+      //   console.log('Error: ' + reason);
+      //   loaderService.closeLoader();
+      // });
 
     }
   };
