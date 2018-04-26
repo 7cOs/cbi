5c6ea5fab848bde0cd55c0aa2cbb1cab51bd1f5d
Piotr Bogun <pbogun1123@gmail.com>
2018-04-26 18:26:34 -0500

    US23807 - initial styles

---
+++ app/models/lists/list-table-drawer-row.model.ts
@@ -0,0 +1,12 @@
+export interface ListTableDrawerRow {
+  mandate: string;
+  brand: string;
+  skuPackage: string;
+  type: string;
+  status: string;
+  impact: string;
+  current: string;
+  yearAgo: string;
+  depletionDate: string;
+  checked: boolean;
+}
--- app/shared/components/list-table-drawer/list-table-drawer.component.ts
+++ app/shared/components/list-table-drawer/list-table-drawer.component.ts
@@ -6,4 +6,32 @@ import { Component } from '@angular/core';
   styles: [require('./list-table-drawer.component.scss')]
 })
 
-export class ListTableDrawerComponent { }
+export class ListTableDrawerComponent {
+  public opportunitiesData: any = [{
+    mandate: 'SP',
+    brand: 'Corona Extra',
+    skuPackage: '16OZ 6PK CAN',
+    type: 'Low Velocity',
+    status: 'Closed',
+    impact: 'high',
+    current: '12,212',
+    yearAgo: '4.5%',
+    depletionDate: '02/21/2018',
+    checked: false
+  }, {
+    mandate: null,
+    brand: 'TOCAYO IPA',
+    skuPackage: 'ANY',
+    type: 'New Placement (No Rebuy)',
+    status: 'Targeted',
+    impact: 'low',
+    current: '12,212',
+    yearAgo: '4.5%',
+    depletionDate: '03/14/2018',
+    checked: true
+  }];
+
+  public onCheckboxClick(isChecked: boolean, index: number): void {
+    this.opportunitiesData[index].checked = isChecked;
+  }
+}
