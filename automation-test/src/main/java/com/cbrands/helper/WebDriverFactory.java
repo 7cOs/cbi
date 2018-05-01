3e4865889d7abf495e0e9f911e25ea5ff15fde1f
SKARNEH <SKARNEH@PC0QMY9T.cwc.cbi.net>
2018-05-01 12:28:48 -0500

    Method 'getSauceWebDriver' code modified in attempts to debug browser failure on circleCi

--- automation-test/src/main/java/com/cbrands/helper/WebDriverFactory.java
+++ automation-test/src/main/java/com/cbrands/helper/WebDriverFactory.java
@@ -86,13 +86,13 @@ public class WebDriverFactory implements SauceOnDemandSessionIdProvider, SauceOn
     final DesiredCapabilities capabilities = getSauceCapabilitiesByBrowser(testName, System.getProperty("browser"));
     SauceHelpers.addSauceConnectTunnelId(capabilities);
 
+    System.out.println( "Inside getSauceWebDriver... Capabilities: " +  capabilities );
+    
     // Launch remote browser and set it as the current thread
     webDriver.set(new RemoteWebDriver(
       new URL("https://" + authentication.getUsername() + ":" + authentication.getAccessKey() +
           SauceHelpers.buildSauceUri() + "/wd/hub"), capabilities));
     
-    System.out.println( "Inside getSauceWebDriver... WebDriver: " +  webDriver.get() );
-
     //((RemoteWebDriver) getWebDriver()).manage().window().setSize(new Dimension(1024, 768));
     // set current sessionId
     String id = ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
