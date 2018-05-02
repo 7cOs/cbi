21229b9d9ba579e361046905f0c996e03288ee00
SKARNEH <SKARNEH@PC0QMY9T.cwc.cbi.net>
2018-05-01 21:26:19 -0500

    Method 'getSauceWebDriver' code modified in attempts to debug browser failure

--- automation-test/src/main/java/com/cbrands/helper/WebDriverFactory.java
+++ automation-test/src/main/java/com/cbrands/helper/WebDriverFactory.java
@@ -83,12 +83,12 @@ public class WebDriverFactory implements SauceOnDemandSessionIdProvider, SauceOn
   }
 
   private static WebDriver getSauceWebDriver(String testName) throws MalformedURLException {
-    // final DesiredCapabilities capabilities = getSauceCapabilitiesByBrowser(testName, System.getProperty("browser"));
-    // SauceHelpers.addSauceConnectTunnelId(capabilities);
+    final DesiredCapabilities capabilities = getSauceCapabilitiesByBrowser(testName, System.getProperty("browser"));
+    SauceHelpers.addSauceConnectTunnelId(capabilities);
 
-    DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+    /*DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
     capabilities.setCapability("platform", "Windows 10");
-    capabilities.setCapability("version", "dev");
+    capabilities.setCapability("version", "dev");*/
     
     // Launch remote browser and set it as the current thread
     webDriver.set(new RemoteWebDriver(
