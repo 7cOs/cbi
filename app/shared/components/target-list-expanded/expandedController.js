9cbb6dffb10beb6d81287a81efff9193694b8715
Cody Knauer <codyknauer@gmail.com>
2018-05-06 17:30:51 -0500

    US23707 - update expandedController specs with v3 lists endpoints

--- app/shared/components/target-list-expanded/expandedController.js
+++ app/shared/components/target-list-expanded/expandedController.js
@@ -61,6 +61,7 @@ module.exports = /*  @ngInject */
     vm.addCollaborator = addCollaborator;
     vm.archiveTargetList = archiveTargetList;
     vm.closeModal = closeModal;
+    vm.createList = createList;
     vm.createNewList = createNewList;
     vm.createTargetList = createTargetList;
     vm.deleteTargetList = deleteTargetList;
@@ -208,22 +209,29 @@ module.exports = /*  @ngInject */
       return result;
     }
 
-    function saveNewList(e) {
+    function createList(formattedList) {
+      console.log('in the actual thing');
+      return listsApiService.createList(formattedList).toPromise();
+    }
 
+    function saveNewList(e) {
+      console.log('savenewlist');
       if (vm.newList.name.length > 40) return;
 
       vm.buttonDisabled = true;
 
-      listsApiService.createList(listsTransformerService.formatNewList(vm.newList))
-        .toPromise()
+      const formattedList = listsTransformerService.formatNewList(vm.newList);
+      vm.createList(formattedList)
         .then(v3List => {
 
-        userService.model.ownedNotArchivedTargetLists.concat(v3List);
-        userService.model.ownedNotArchived++;
+        userService.model.targetLists.ownedNotArchivedTargetLists.concat(v3List);
+        userService.model.targetLists.ownedNotArchived++;
 
+        console.log(v3List.id);
         closeModal();
         vm.buttonDisabled = false;
 
+        console.log('callback');
         analyticsService.trackEvent('Target Lists - My Target Lists', 'Create Target List', v3List.id);
 
         // reset model
