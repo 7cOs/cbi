390a76dcf7d58e882cd74976fb51ce89d570a6ea
wickerpopstar <patrick.b.powers@gmail.com>
2018-05-07 18:44:16 -0500

    US23706 - add CSS color to percent, adjust styling, rearrange component

--- app/shared/components/list-performance-summary/list-performance-summary.component.ts
+++ app/shared/components/list-performance-summary/list-performance-summary.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CompassListClassUtilService } from '../../../services/compass-list-class-util.service';
 
 @Component({
     selector: 'list-performance-summary',
     template: require('./list-performance-summary.component.pug'),
     styles: [require('./list-performance-summary.component.scss')]
 })
-export class ListPerformanceSummaryComponent implements OnInit, OnChanges {
+export class ListPerformanceSummaryComponent implements OnInit {
     @Input() depletionsTotal: number;
     @Input() depletionsVsYA: number;
     @Input() distributionsTotal: number;
@@ -13,11 +14,18 @@ export class ListPerformanceSummaryComponent implements OnInit, OnChanges {
 
     public depletionsTitle: string = 'DEPLETIONS CYTD';
     public distributionsTitle: string = 'EFFECTIVE POD L90';
+    public depletionsVsYAColorClass: string;
+    public distributionsVsYAColorClass: string;
 
-    ngOnChanges(changes: SimpleChanges): void {
-        throw new Error('Method not implemented.');
-    }
+    private classUtilService: CompassListClassUtilService = new CompassListClassUtilService();
+
     ngOnInit(): void {
-        throw new Error('Method not implemented.');
+        this.depletionsVsYAColorClass = this.getPercentColorClass(this.depletionsVsYA);
+        this.distributionsVsYAColorClass = this.getPercentColorClass(this.distributionsVsYA);
+    }
+
+    public getPercentColorClass(percent: number): string {
+        percent = percent || 0;
+        return this.classUtilService.getTrendClass(percent);
     }
 }
