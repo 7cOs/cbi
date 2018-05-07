c7a9d12112c22c7e7ec200ced257a7d9aa178e0f
Jeremy Kalinowski <jeremy.kalinowski@cbrands.com>
2018-05-07 12:22:26 -0400

    US23625 - Added tests for the user search component.

--- app/shared/components/compass-user-search/compass-user-search.component.spec.ts
+++ app/shared/components/compass-user-search/compass-user-search.component.spec.ts
@@ -7,7 +7,7 @@ import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, FormBuilder }
 
 const chance = new Chance();
 
-fdescribe('Compass User Search Component', () => {
+describe('Compass User Search Component', () => {
   let fixture: ComponentFixture<CompassUserSearchComponent>;
   let componentInstance: CompassUserSearchComponent;
   let fixtureDebugElement: DebugElement;
@@ -41,6 +41,8 @@ fdescribe('Compass User Search Component', () => {
     });
 
     fixture = TestBed.createComponent(CompassUserSearchComponent);
+    fixture.componentInstance.showX = true;
+    fixture.componentInstance.selectedResult = {user: 'test'};
     componentInstance = fixture.componentInstance;
     componentInstance.parentGroup = formBuilder.group({userSearchTerm: ''});
     fixture.detectChanges();
@@ -56,7 +58,38 @@ fdescribe('Compass User Search Component', () => {
       componentInstance.parentGroup.controls['userSearchTerm'].setValue('tests');
       componentInstance.callSearch();
       fixture.detectChanges();
-      expect(componentInstance.searchResults).toBe([{value: 'test'}]);
+      fixture.whenStable().then(() => {
+        expect(componentInstance.searchResults).toEqual([{value: 'test'}]);
+      });
+    });
+  });
+
+  describe('clearModel function', () => {
+    it('should clear the form input and selected result and close button', () => {
+      componentInstance.parentGroup.controls['userSearchTerm'].setValue('tests');
+      componentInstance.clearModel();
+      fixture.detectChanges();
+      expect(componentInstance.showX).toBeFalsy();
+      expect(componentInstance.parentGroup.controls['userSearchTerm'].value).toEqual('');
+      expect(componentInstance.selectedResult).toEqual( null );
+    });
+  });
+
+  describe('resultChosen function', () => {
+    it('should select a user and emit an event and call close to hide the ui', () => {
+      let testUser = {user: 'test'};
+      componentInstance.resultChosen(testUser);
+      fixture.detectChanges();
+
+      componentInstance.addedCollaboratorEvent.subscribe((value: object) => {
+        expect(value).toEqual(testUser);
+      });
+
+      expect(componentInstance.showX).toBeFalsy();
+      expect(componentInstance.showSearchIcon).toBeFalsy();
+      expect(componentInstance.selectedResult).toEqual({});
+      expect(componentInstance.searchResults).toEqual([]);
+      expect(componentInstance.showResults).toBeFalsy();
     });
   });
 
