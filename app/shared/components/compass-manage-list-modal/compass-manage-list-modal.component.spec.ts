62d06286f2e2b961044d11796c9ea7e3dd756548
Jeremy Kalinowski <jeremy.kalinowski@cbrands.com>
2018-05-08 09:17:44 -0400

    US23625 - Clean up of widgets and tests.

--- app/shared/components/compass-manage-list-modal/compass-manage-list-modal.component.spec.ts
+++ app/shared/components/compass-manage-list-modal/compass-manage-list-modal.component.spec.ts
@@ -1,83 +1,68 @@
 import * as Chance from 'chance';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { DebugElement, Component, Input, NO_ERRORS_SCHEMA } from '@angular/core';
+import { DebugElement, Component } from '@angular/core';
 
 import { CompassManageListModalComponent } from './compass-manage-list-modal.component';
+import { CompassManageListModalEvent } from '../../../enums/compass-manage-list-modal-strings.enum';
 import { CompassManageListModalInputs } from '../../../models/compass-manage-list-modal-inputs.model';
 import { COMPASS_MANAGE_LIST_MODAL_INPUTS } from './compass-manage-list-modal.tokens';
-import { CompassManageListModalEvent } from '../../../enums/compass-manage-list-modal-strings.enum';
-import { ListsSummary } from '../../../models/lists/lists-header.model';
-import { User, CollaboratorOwnerDetails } from '../../../models/lists/lists.model';
-import { FormsModule, ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
 import { CompassModalService } from '../../../services/compass-modal.service';
+import { CompassUserSearchComponent } from '../compass-user-search/compass-user-search.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ListsSummary } from '../../../models/lists/lists-header.model';
 
 const chance = new Chance();
-const modalOverlayRefMock = {
-  closeModal: jasmine.createSpy('closeModal')
+
+const searchServiceMock = {
+  getUsers: jasmine.createSpy('getUsers').and.callFake((arg: any) => {
+    return new Promise((resolve, reject) => {
+      resolve([{
+        value: 'test'
+      }]);
+    });
+  }),
+  setSearchActive: jasmine.createSpy('setSearchActive').and.callFake((arg: any) => {
+    return true;
+  })
 };
 
-fdescribe('Compass Manage Modal List Component', () => {
+describe('Compass Manage Modal List Component', () => {
   let fixture: ComponentFixture<CompassManageListModalComponent>;
   let componentInstance: CompassManageListModalComponent;
-  let fixtureDebugElement: DebugElement;
-  let compassModalInputsMock: CompassManageListModalInputs;
   let compassModalService: CompassModalService;
-  let formBuilder: FormBuilder = new FormBuilder();
 
-  let titleInputMock: string;
-  let acceptLabelMock: string;
-  let rejectLabelMock: string;
-  let listObjectMock: ListsSummary;
-  let currentUserMock: User;
-  let searchServiceMock = {
-    getUsers: getUsers,
-    setSearchActive: setSearchActive
-  };
-
-  function getUsers (value: string) {
-    return new Promise((resolve, reject) => {
-      resolve([{value: 'test'}]);
-    });
-  }
-  function setSearchActive() {
-    return true;
-  }
+  let compassModalInputsMock: CompassManageListModalInputs;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule, FormsModule],
-      declarations: [ CompassManageListModalComponent ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule
+      ],
+      declarations: [
+        CompassManageListModalComponent,
+        CompassUserSearchComponent
+      ],
       providers: [
         {
           provide: COMPASS_MANAGE_LIST_MODAL_INPUTS,
           userValue: compassModalInputsMock
-        },
-        {
-          provide: FormBuilder,
-          useValue: formBuilder
-        },
-        {
-          provide: 'searchService',
-          useValue: searchServiceMock
-        },
-        {
+        }, {
           provide: CompassModalService,
           useValue: compassModalService
+        }, {
+          provide: 'searchService',
+          useValue: searchServiceMock
         }
-      ],
-      schemas: [ NO_ERRORS_SCHEMA ]
+      ]
     });
 
-    titleInputMock = chance.string();
-    acceptLabelMock = chance.string();
-    rejectLabelMock = chance.string();
-    currentUserMock = {
-      firstName: 'Bob',
-      lastName: 'Bobby',
-      employeeId: chance.string()
-    };
-    listObjectMock = {
+    compassModalInputsMock = {
+      title: chance.string(),
+      acceptLabel: chance.string(),
+      rejectLabel: chance.string(),
+      listObject: {
         archived: false,
         description: chance.string(),
         id: chance.string(),
@@ -92,72 +77,57 @@ fdescribe('Compass Manage Modal List Component', () => {
         collaboratorType: null,
         category: null,
         type: null
-    };
-
-    compassModalInputsMock = {
-      title: titleInputMock,
-      acceptLabel: acceptLabelMock,
-      rejectLabel: rejectLabelMock,
-      listObject: listObjectMock,
-      currentUser: currentUserMock
+      },
+      currentUser: {
+        firstName: chance.string(),
+        lastName: chance.string(),
+        employeeId: chance.string()
+      }
     };
 
     fixture = TestBed.createComponent(CompassManageListModalComponent);
     componentInstance = fixture.componentInstance;
-    componentInstance.listForm = formBuilder.group({targetName: '', description: '', userSearchTerm: ''});
+
     componentInstance.modalInputs = compassModalInputsMock;
-    componentInstance.modalOverlayRef = modalOverlayRefMock as any;
-    fixtureDebugElement = fixture.debugElement;
 
     fixture.detectChanges();
   });
 
   describe('Compass Manage List Modal Inputs', () => {
     it('should contain a title element when the injection has a title', () => {
-      let titleElement: DebugElement = fixture.debugElement.query(By.css('.compass-modal-title'));
+      const titleElement: DebugElement = fixture.debugElement.query(By.css('.compass-modal-title'));
+
       expect(titleElement.nativeElement.textContent).toEqual(compassModalInputsMock.title);
 
       componentInstance.modalInputs.title = undefined;
       fixture.detectChanges();
-      titleElement = fixture.debugElement.query(By.css('.compass-modal-title'));
+
       expect(titleElement.nativeElement.textContent).toEqual('');
     });
+
     it('should contain a cancel button element when the injection has a cancel button string', () => {
-      let buttonElement: DebugElement = fixture.debugElement.query(By.css('.compass-modal-btn'));
+      const buttonElement: DebugElement = fixture.debugElement.query(By.css('.compass-modal-btn'));
+
       expect(buttonElement.nativeElement.textContent).toEqual(compassModalInputsMock.rejectLabel);
     });
+
     it('should contain an accept button element when the injection has an accept button string', () => {
-      let buttonElement: DebugElement = fixture.debugElement.query(By.css('.btn-action'));
+      const buttonElement: DebugElement = fixture.debugElement.query(By.css('.btn-action'));
+
       expect(buttonElement.nativeElement.textContent).toEqual(compassModalInputsMock.acceptLabel);
     });
   });
 
   describe('Compass Manage List Modal Outputs', () => {
-    it('should output an accept message when the accept button is clicked', () => {
-      let buttonElement: DebugElement = fixture.debugElement.query(By.css('.btn-action'));
+    it('should output an accept message when the accept button is clicked', (done) => {
+      const buttonElement: DebugElement = fixture.debugElement.query(By.css('.btn-action'));
+
       componentInstance.buttonContainerEvent.subscribe((value: object) => {
-        expect(value).toEqual(listObjectMock);
+        expect(value).toEqual(compassModalInputsMock.listObject);
+        done();
       });
 
       buttonElement.nativeElement.click();
-      expect(componentInstance.modalOverlayRef.closeModal).toHaveBeenCalled();
-
-    });
-  });
-  describe('Compass Manage List Modal Add Collaborator', () => {
-    it('should output an accept message when the accept button is clicked', () => {
-      let buttonElement: DebugElement = fixture.debugElement.query(By.css('.btn-action'));
-      componentInstance.addCollaborator({employeeId: chance.string(),
-        firstName: chance.string(),
-        lastName: chance.string()
-      });
-      componentInstance.buttonContainerEvent.subscribe((value: ListsSummary) => {
-        expect(value.collaborators.length).toEqual(1);
-      });
-
-      buttonElement.nativeElement.click();
-      expect(componentInstance.modalOverlayRef.closeModal).toHaveBeenCalled();
-
     });
   });
 });
--- app/shared/components/compass-manage-list-modal/compass-manage-list-modal.component.ts
+++ app/shared/components/compass-manage-list-modal/compass-manage-list-modal.component.ts
@@ -1,16 +1,15 @@
-import { Component, EventEmitter, HostListener, Output, Inject, Input } from '@angular/core';
+import { Component, EventEmitter, HostListener, Output, Inject, Input, OnInit } from '@angular/core';
 
+import { CompassAlertModalEvent } from '../../../enums/compass-alert-modal-strings.enum';
 import { CompassManageListModalInputs } from '../../../models/compass-manage-list-modal-inputs.model';
-import { CompassManageListModalOverlayRef } from './compass-manage-list-modal.overlayref';
 import { COMPASS_MANAGE_LIST_MODAL_INPUTS } from '../../components/compass-manage-list-modal/compass-manage-list-modal.tokens';
 import { CompassManageListModalEvent } from '../../../enums/compass-manage-list-modal-strings.enum';
-import { FormsModule, FormGroup, FormBuilder } from '@angular/forms';
-import { FormControl } from '@angular/forms';
-import { ListsSummary } from '../../../models/lists/lists-header.model';
-import { CompassAlertModalEvent } from '../../../enums/compass-alert-modal-strings.enum';
+import { CompassManageListModalOverlayRef } from './compass-manage-list-modal.overlayref';
 import { CompassModalService } from '../../../services/compass-modal.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ListsSummary } from '../../../models/lists/lists-header.model';
 
-const ESCKEY = 27;
+const ESCKEY: number = 27;
 
 @Component({
   selector: 'compass-manage-list-modal',
@@ -18,22 +17,24 @@ const ESCKEY = 27;
   styles: [require('./compass-manage-list-modal.component.scss')]
 })
 
-export class CompassManageListModalComponent {
+export class CompassManageListModalComponent implements OnInit {
   @Output() buttonContainerEvent = new EventEmitter<ListsSummary>();
+
   public modalOverlayRef: CompassManageListModalOverlayRef;
   public compassManageListModalEvent = CompassManageListModalEvent;
   public listForm: FormGroup;
   public collaborators: Array<object> = [];
   public archiveModalStringInputs = {
-    'title': 'Are you sure?',
-    'body': 'By archiving this list, only limited set functionality will remain available.',
-    'rejectLabel': 'Cancel',
-    'acceptLabel': 'Archive'};
-  public deleteModalStringInputs =  {
-    'title': 'Are you sure?',
-    'body': 'Deleting a list cannot be undone. You\'ll lose all list store performance and opportunity progress.',
-    'rejectLabel': 'Cancel',
-    'acceptLabel': 'Delete'
+    title: 'Are you sure?',
+    body: 'By archiving this list, only limited set functionality will remain available.',
+    rejectLabel: 'Cancel',
+    acceptLabel: 'Archive'
+  };
+  public deleteModalStringInputs = {
+    title: 'Are you sure?',
+    body: 'Deleting a list cannot be undone. You\'ll lose all list store performance and opportunity progress.',
+    rejectLabel: 'Cancel',
+    acceptLabel: 'Delete'
   };
 
   constructor(
@@ -41,30 +42,26 @@ export class CompassManageListModalComponent {
     @Inject('searchService') private searchService: any,
     private fb: FormBuilder,
     public compassModalService: CompassModalService
-  ) {
-    if (modalInputs.listObject) {
-      this.listForm = this.fb.group({
-        targetName: modalInputs.listObject.name,
-        description: modalInputs.listObject.description,
-        userSearchTerm: ''
-      });
+  ) { }
 
-      this.listForm.setValue({
-        targetName: modalInputs.listObject.name,
-        description: modalInputs.listObject.description,
-        userSearchTerm: ''
-      });
+  ngOnInit() {
+    this.listForm = this.fb.group({
+      targetName: this.modalInputs.listObject.name,
+      description: this.modalInputs.listObject.description,
+      userSearchTerm: ''
+    });
 
-      this.collaborators = Object.assign([], modalInputs.listObject.collaborators);
-    }
+    this.listForm.setValue({
+      targetName: this.modalInputs.listObject.name,
+      description: this.modalInputs.listObject.description,
+      userSearchTerm: ''
+    });
+
+    this.collaborators = Object.assign([], this.modalInputs.listObject.collaborators);
   }
 
   @HostListener('document:keydown', ['$event']) public handleKeydown(event: KeyboardEvent) {
-    if (event.keyCode === ESCKEY) {
-      if (this.modalOverlayRef) {
-        this.modalOverlayRef.closeModal();
-      }
-    }
+    if (event.keyCode === ESCKEY) this.modalOverlayRef.closeModal();
   }
 
   public addCollaborator(collaborator: object) {
@@ -72,19 +69,17 @@ export class CompassManageListModalComponent {
   }
 
   public convertFormPayload(): ListsSummary {
-    let payload = Object.assign({}, this.modalInputs.listObject);
-    payload.name = this.listForm.get('targetName').value;
-    payload.description = this.listForm.get('description').value;
-    payload.collaborators = this.collaborators;
-    return payload;
+    return Object.assign({}, this.modalInputs.listObject, {
+      name: this.listForm.get('targetName').value,
+      description: this.listForm.get('description').value,
+      collaborators: this.collaborators
+    });
   }
+
   public hideModal(modalEventString: CompassManageListModalEvent): void {
-    if ( modalEventString === CompassManageListModalEvent.Accept) {
-      this.buttonContainerEvent.emit(this.convertFormPayload());
-    }
-    if (this.modalOverlayRef) {
-      this.modalOverlayRef.closeModal();
-    }
+    if (modalEventString === CompassManageListModalEvent.Accept) this.buttonContainerEvent.emit(this.convertFormPayload());
+
+    this.modalOverlayRef.closeModal();
   }
 
   public showArchiveModal() {
@@ -104,5 +99,4 @@ export class CompassManageListModalComponent {
       }
     });
   }
-
 }
