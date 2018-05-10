5078ef7d6be0c92dcd4ff1ae0e9c12bd558a80e8
wickerpopstar <patrick.b.powers@gmail.com>
2018-05-10 17:03:36 -0500

    DE7598 - add missing method to mock

--- app/services/lists-transformer.service.mock.ts
+++ app/services/lists-transformer.service.mock.ts
@@ -1,10 +1,13 @@
 import { FormattedNewList } from '../models/lists/formatted-new-list.model';
-import { getFormattedNewList, getListsCollectionSummaryMock } from '../models/lists/lists.model.mock';
+import { getFormattedNewList, getListsCollectionSummaryMock, getV2ListMock } from '../models/lists/lists.model.mock';
 import { ListsCollectionSummary } from '../models/lists/lists-collection-summary.model';
+import { V2List } from '../models/lists/v2-list.model';
+import { V3List } from '../models/lists/v3-list.model';
 
 export const listsTransformerServiceMock = {
   formatNewList: formatNewList,
-  getV2ListsSummary: getV2ListsSummary
+  getV2ListsSummary: getV2ListsSummary,
+  transformV3ToV2: transformV3ToV2
 };
 
 function formatNewList(...args: any[]): FormattedNewList  {
@@ -13,4 +16,8 @@ function formatNewList(...args: any[]): FormattedNewList  {
 
 function getV2ListsSummary(...args: any[]): ListsCollectionSummary {
   return getListsCollectionSummaryMock();
+}
+
+function transformV3ToV2(...args: any[]): V2List {
+  return getV2ListMock();
 }
