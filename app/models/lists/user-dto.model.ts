7ba09a337b0ed4fb42143906fe24c6e7bb722ac3
Jeremy Kalinowski <jeremy.kalinowski@cbrands.com>
2018-05-11 15:27:42 -0400

    US23625 - Added user DTO model.

---
+++ app/models/lists/user-dto.model.ts
@@ -0,0 +1,9 @@
+export interface UserDTO {
+  accounts?: Array<string>;
+  roles?: Array<string>;
+  employeeId: string;
+  firstName: string;
+  lastName: string;
+  email?: string;
+  id?: string;
+}
