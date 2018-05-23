84424e98d6c845b167a766bb13c8e0c221a37f13
Edie Liao <ediel@slalom.com>
2018-05-23 15:06:41 -0500

    US24687- Extracting SauceDriverFactory to its own class file

--- automation-test/src/main/java/com/cbrands/helper/WebDriverFactory.java
+++ automation-test/src/main/java/com/cbrands/helper/WebDriverFactory.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import com.cbrands.helper.driver.factory.LocalDriverFactory;
+import com.cbrands.helper.driver.factory.SauceDriverFactory;
 import org.apache.commons.lang3.Validate;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -88,45 +89,6 @@ public class WebDriverFactory implements SauceOnDemandSessionIdProvider, SauceOn
       TEST_TRIGGER_ORIGIN_NAME,
       testName
     );
-  }
-
-  public static class SauceDriverFactory {
-    private static final String SAUCELABS_URI_FORMAT = "http://%s:%s@ondemand.saucelabs.com:%d/wd/hub";
-    private static final int DEFAULT_SAUCE_PORT = 80;
-
-    public static RemoteWebDriver getRemoteDriverForChrome(String formattedTestRunName) throws MalformedURLException {
-      return new RemoteWebDriver(buildSauceURI(), getDesiredCapabilitiesForChrome(formattedTestRunName));
-    }
-
-    public static RemoteWebDriver getRemoteDriverForIE(String formattedTestRunName) throws MalformedURLException {
-      return new RemoteWebDriver(buildSauceURI(), getDesiredCapabilitiesForIE(formattedTestRunName));
-    }
-
-    private static DesiredCapabilities getDesiredCapabilitiesForChrome(String formattedTestRunName) {
-      final DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-      capabilities.setCapability(CapabilityType.PLATFORM, Platform.WIN8_1);
-      capabilities.setCapability("name", formattedTestRunName);
-      return capabilities;
-    }
-
-    private static DesiredCapabilities getDesiredCapabilitiesForIE(String formattedTestRunName) {
-      final DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
-      capabilities.setCapability(CapabilityType.PLATFORM, Platform.WIN8_1);
-      capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-      capabilities.setCapability(InternetExplorerDriver.REQUIRE_WINDOW_FOCUS, true);
-      capabilities.setCapability(InternetExplorerDriver.ENABLE_ELEMENT_CACHE_CLEANUP, true);
-      capabilities.setCapability(InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION, true);
-      capabilities.setCapability("name", formattedTestRunName);
-      return capabilities;
-    }
-
-    private static URL buildSauceURI() throws MalformedURLException {
-      final String username = PropertiesCache.getInstance().getProperty("sauce.userName");
-      final String accessKey = PropertiesCache.getInstance().getProperty("sauce.accessKey");
-
-      return new URL(String.format(SAUCELABS_URI_FORMAT, username, accessKey, DEFAULT_SAUCE_PORT));
-    }
-
   }
 
   public enum BrowserType {
